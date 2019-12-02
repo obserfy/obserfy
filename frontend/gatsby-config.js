5ccbb8e83e88bd86bd7dbd856b0d711aaaf2c3d0
Chrisando Pramudhita <chrisep8@gmail.com>
2019-12-02 22:02:32 +0700

    use emotion and offline gatsby plugin. Add Icon.

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -23,15 +23,17 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `vor`,
-        short_name: `vor`,
+        name: `Vör`,
+        short_name: `Vör`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.svg`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-emotion`,
     {
       resolve: "gatsby-plugin-svgr",
       options: {
