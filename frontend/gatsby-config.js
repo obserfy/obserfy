071577558e2ba396427600616fbc946bd656ca3d
Chrisando Pramudhita <chrsep@protonmail.com>
2019-12-09 23:17:27 +0700

    feat(PWA): Make frontend act as standalone PWA.

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
+        display: `standalone`,
         icon: `src/images/icon.svg`, // This path is relative to the root of the site.
       },
     },
