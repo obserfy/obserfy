51bd5c6f2c80119f548093b81d0f2e4168d9f8ab
Chrisando Pramudhita <chrsep@protonmail.com>
2020-01-16 16:13:05 +0700

    fix(storybook): Fix various storybook issue, upgrade deps.

--- frontend/src/components/NewStudentDialog/NewStudentDialog.stories.tsx
+++ frontend/src/components/NewStudentDialog/NewStudentDialog.stories.tsx
@@ -1,12 +1,15 @@
 import React, { FC } from "react"
+import { action } from "@storybook/addon-actions"
 import NewStudentDialog from "./NewStudentDialog"
 
 export default {
   title: "Core|NewStudentDialog",
   component: NewStudentDialog,
   parameters: {
-    componentSubtitle: "Just a simple NewStudentDialog"
-  }
+    componentSubtitle: "Just a simple NewStudentDialog",
+  },
 }
 
-export const Basic: FC = () => <NewStudentDialog />
+export const Basic: FC = () => (
+  <NewStudentDialog onCancel={action("Cancel")} onConfirm={action("Confirm")} />
+)
