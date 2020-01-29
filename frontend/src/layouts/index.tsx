058f775b8d14e7b4040b3576325bf3b76db4ba8b
Chrisando Pramudhita <chrsep@protonmail.com>
2020-01-29 21:57:30 +0700

    fix(frontend): Fix broken background color. (#28)

--- frontend/src/layouts/index.tsx
+++ frontend/src/layouts/index.tsx
@@ -1,6 +1,8 @@
 import React, { FC, useState } from "react"
+import { Global } from "@emotion/core"
 import Layout from "../components/Layout/Layout"
 import ErrorBoundary from "../components/ErrorBoundary/ErrorBoundary"
+import Box from "../components/Box/Box"
 
 export const PageTitleContext = React.createContext({
   title: "",
@@ -11,15 +13,31 @@ export const PageTitleContext = React.createContext({
 const LayoutManager: FC<any> = ({ children, pageContext }) => {
   const [title, setTitle] = useState("Home")
   if (pageContext.layout === "open") {
-    return children
+    return (
+      <ErrorBoundary title={title}>
+        <GlobalStyle />
+        <Box backgroundColor="background">{children}</Box>
+      </ErrorBoundary>
+    )
   }
   return (
     <PageTitleContext.Provider value={{ title, setTitle }}>
+      <GlobalStyle />
       <ErrorBoundary title={title}>
         <Layout pageTitle={title}>{children}</Layout>
       </ErrorBoundary>
     </PageTitleContext.Provider>
   )
 }
+
+const GlobalStyle: FC = () => (
+  <Global
+    styles={theme => ({
+      body: {
+        backgroundColor: theme.colors.background,
+      },
+    })}
+  />
+)
 
 export default LayoutManager
