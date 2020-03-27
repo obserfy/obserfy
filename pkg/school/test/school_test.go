1f241c29edfe6b04e40cc69aa8750cc8c4b19bf1
Chrisando Pramudhita <chrsep@protonmail.com>
2020-03-28 01:22:52 +0700

    Fix broken observation test.

--- pkg/school/test/school_test.go
+++ pkg/school/test/school_test.go
@@ -27,38 +27,6 @@ func TestObservation(t *testing.T) {
 	suite.Run(t, new(SchoolTestSuite))
 }
 
-func (s *SchoolTestSuite) SaveNewSchool() *postgres.School {
-	t := s.T()
-	gofakeit.Seed(time.Now().UnixNano())
-	newUser := postgres.User{Id: uuid.New().String()}
-	newSchool := postgres.School{
-		Id:           uuid.New().String(),
-		Name:         gofakeit.Name(),
-		InviteCode:   uuid.New().String(),
-		Users:        []postgres.User{},
-		CurriculumId: "",
-		Curriculum:   postgres.Curriculum{},
-	}
-	newSchool.Users = []postgres.User{newUser}
-	schoolUserRelation := postgres.UserToSchool{
-		SchoolId: newSchool.Id,
-		UserId:   newUser.Id,
-	}
-	if err := s.DB.Insert(&newUser); err != nil {
-		assert.NoError(t, err)
-		return nil
-	}
-	if err := s.DB.Insert(&newSchool); err != nil {
-		assert.NoError(t, err)
-		return nil
-	}
-	if err := s.DB.Insert(&schoolUserRelation); err != nil {
-		assert.NoError(t, err)
-		return nil
-	}
-	return &newSchool
-}
-
 func (s *SchoolTestSuite) SaveNewClass(school postgres.School) *postgres.Class {
 	t := s.T()
 	newClass := postgres.Class{
