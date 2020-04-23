688d5b52731ed2bd8f97305d70dca147a922c1c4
Chrisando Pramudhita <chrsep@protonmail.com>
2020-04-23 15:31:16 +0700

    Add test for class management.

--- frontend/cypress/.eslintrc.js
+++ frontend/cypress/.eslintrc.js
@@ -1,11 +1,17 @@
 module.exports = {
   parser: "@typescript-eslint/parser", // Specifies the ESLint parser
-  plugins: ["@typescript-eslint", "cypress", "prettier"],
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  plugins: ["cypress"],
   extends: [
-    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    "prettier",
+    "airbnb-typescript",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "plugin:prettier/recommended",
     "prettier/@typescript-eslint",
-    "plugin:prettier/recommended", // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    "prettier/react",
   ],
   rules: {
     "jest/valid-expect-in-promise": 0,
