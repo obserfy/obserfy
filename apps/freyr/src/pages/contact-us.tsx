e629de60f1a867850ccfd14cf9bd09b70952652e
Tiffany <tiff.wijaya@gmail.com>
2020-05-03 22:05:20 +0700

    feat: add form contact us

--- apps/freyr/src/pages/contact-us.tsx
+++ apps/freyr/src/pages/contact-us.tsx
@@ -1,6 +1,5 @@
 import React, { FC } from "react"
 
-import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Button from "../components/button"
@@ -8,26 +7,36 @@ import Button from "../components/button"
 const ContactUsPage: FC = () => (
   <Layout>
     <SEO title="Home" />
-    <div className="flex-row justify-center my-16 max-w-xl">
-      <h1 className="text-5xl mb-4 leading-none font-heading font-medium">
-        Contact Us
-      </h1>
-      <p className="text-xl my-8 text-gray-700 font-body">
-        Record, manage and analyze your student observations collaboratively.
-        Plan future lessons, compile reports, in one platform.
-      </p>
-      <div className="sm:flex">
-        <a
-          href="https://app.obserfy.com/register"
-          className="block mb-3 sm:mb-0 sm:mr-3"
-        >
-          <Button className="w-full sm:w-auto text-lg">Get Started</Button>
-        </a>
-        <Link to="/contact-us">
-          <Button secondary className="w-full sm:w-auto text-lg bg-green-200">
-            Contact Us
-          </Button>
-        </Link>
+    <div className="flex sm:flex-box md:flex lg:flex my-16">
+      <div className="flex-row justify-center lg:w-1/2">
+        <h1 className="text-5xl mb-4 leading-none font-heading font-medium">
+          Contact Us
+        </h1>
+        <p className="text-xl my-8 text-gray-700 font-body">
+          Record, manage and analyze your student observations collaboratively.
+          Plan future lessons, compile reports, in one platform.
+        </p>
+        <div className="sm:flex mb-16">
+          <a
+            href="https://app.obserfy.com/register"
+            className="block mb-3 sm:mb-0 sm:mr-3"
+          >
+            <Button secondary className="w-full sm:w-auto text-lg bg-green-200">Get Started</Button>
+          </a>
+        </div>
+      </div>
+      <div className="flex-row justify-center lg:w-1/3 mx-32">
+        <form className="bg-white shadow-md rounded p-8" name="contact" method="POST" data-netlify="true">
+          <div className="py-4">
+            <input type="text" id="email" name="email" placeholder="Email" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+          </div>
+          <div className="py-4">
+            <textarea name="message" placeholder="Message" className="resize-none border rounded shadow w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+          </div>
+          <div className="py-4">
+            <Button className="w-full text-lg" type="submit">Send</Button>
+          </div>
+        </form>
       </div>
     </div>
   </Layout>
