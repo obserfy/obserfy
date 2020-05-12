63258bd9b7ad2507d8cba57e324bbc79fb25a5e5
Chrisando Pramudhita <chrsep@protonmail.com>
2020-05-13 02:46:51 +0700

    feat: Add lesson plan page.

--- apps/vor/frontend/src/components/PagePlans/PagePlans.tsx
+++ apps/vor/frontend/src/components/PagePlans/PagePlans.tsx
@@ -9,19 +9,39 @@ import { ReactComponent as PrevIcon } from "../../icons/arrow-back.svg"
 import { ReactComponent as NextIcon } from "../../icons/next-arrow.svg"
 
 export const PagePlans: FC = () => {
-  const [currentDate, setCurrentDate] = useState(dayjs)
+  const [selectedDate, setSelectedDate] = useState(dayjs())
 
   return (
-    <Box>
-      <Flex alignItems="flex-end" mx={3} my={4}>
-        <Typography.H6>May 2020</Typography.H6>
-        <Button variant="outline" py={1} px={1} mr={1} ml="auto">
+    <Box maxWidth="maxWidth.sm" mx="auto">
+      <Flex alignItems="center" mx={3} my={3}>
+        <Typography.Body>
+          {selectedDate.format("dddd, DD MMM 'YY")}
+        </Typography.Body>
+        <Button
+          variant="outline"
+          py={1}
+          px={1}
+          mr={1}
+          ml="auto"
+          onClick={() => setSelectedDate(selectedDate.add(-1, "day"))}
+        >
           <Icon as={PrevIcon} m={0} />
         </Button>
-        <Button variant="outline" mr={2} py={1} px={1}>
+        <Button
+          variant="outline"
+          mr={2}
+          py={1}
+          px={1}
+          onClick={() => setSelectedDate(selectedDate.add(1, "day"))}
+        >
           <Icon as={NextIcon} m={0} />
         </Button>
-        <Button variant="outline" py={1} px={3}>
+        <Button
+          variant="outline"
+          py={1}
+          px={3}
+          onClick={() => setSelectedDate(dayjs())}
+        >
           today
         </Button>
       </Flex>
