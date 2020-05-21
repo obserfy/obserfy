2d172dad8cf23cf26866845a57e78006413555ed
Chrisando Pramudhita <chrsep@protonmail.com>
2020-05-22 00:02:42 +0700

    Add plan details page.

--- apps/vor/frontend/src/components/PagePlanDetails/PagePlanDetails.stories.tsx
+++ apps/vor/frontend/src/components/PagePlanDetails/PagePlanDetails.stories.tsx
@@ -1,6 +1,10 @@
 import React, { FC } from "react"
+import { nanoid } from "nanoid"
+import faker from "faker"
 import PagePlanDetails from "./PagePlanDetails"
 import Layout from "../Layout/Layout"
+import mockApi from "../../__mocks__/api"
+import { PlanDetails } from "../../api/useGetPlan"
 
 export default {
   title: "Page|Plans/PagePlanDetails",
@@ -10,12 +14,23 @@ export default {
   },
 }
 
-export const Basic: FC = () => <PagePlanDetails />
+export const Basic: FC = () => <PagePlanDetails id="testid" />
 
 export const WithLayout: FC = () => {
+  mockApi.get(
+    "/plans/*",
+    (): PlanDetails => ({
+      id: nanoid(),
+      title: faker.name.jobTitle(),
+      description: faker.lorem.sentence(20, 2),
+      classId: "asdfasjf",
+      date: "2020-02-01",
+    })
+  )
+
   return (
     <Layout>
-      <PagePlanDetails />
+      <PagePlanDetails id="testid" />
     </Layout>
   )
 }
