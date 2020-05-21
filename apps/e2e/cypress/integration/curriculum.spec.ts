580871a210baa776d87c07314a070962772ff916
Chrisando Pramudhita <chrsep@protonmail.com>
2020-05-22 02:32:46 +0700

    Fix flaky test.

--- apps/e2e/cypress/integration/curriculum.spec.ts
+++ apps/e2e/cypress/integration/curriculum.spec.ts
@@ -39,8 +39,8 @@ describe("Test curriculum features", () => {
     cy.contains("Save").click()
 
     cy.contains("h6", newAreaName).click()
-    cy.wait(100)
-    cy.contains("New").click({ force: true })
+    cy.wait(500)
+    cy.contains("New").click()
 
     cy.contains("Subject name").type(newSubjectName)
 
