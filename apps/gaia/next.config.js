325a8ca1b3aff6893bf71e8098c18c4e89835961
Chrisando Pramudhita <chrsep@protonmail.com>
2020-05-22 22:11:17 +0700

    Use typescript and preact on gaia.

---
+++ apps/gaia/next.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  experimental: {
+    modern: true,
+    polyfillsOptimization: true,
+  },
+
+  webpack(config, { dev, isServer }) {
+    const splitChunks = config.optimization && config.optimization.splitChunks;
+    if (splitChunks) {
+      const cacheGroups = splitChunks.cacheGroups;
+      const preactModules = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/;
+      if (cacheGroups.framework) {
+        cacheGroups.preact = Object.assign({}, cacheGroups.framework, {
+          test: preactModules,
+        });
+        cacheGroups.commons.name = "framework";
+      } else {
+        cacheGroups.preact = {
+          name: "commons",
+          chunks: "all",
+          test: preactModules,
+        };
+      }
+    }
+
+    // Install webpack aliases:
+    const aliases = config.resolve.alias || (config.resolve.alias = {});
+    aliases.react = aliases["react-dom"] = "preact/compat";
+
+    // inject Preact DevTools
+    if (dev && !isServer) {
+      const entry = config.entry;
+      config.entry = () =>
+        entry().then((entries) => {
+          entries["main.js"] = ["preact/debug"].concat(
+            entries["main.js"] || []
+          );
+          return entries;
+        });
+    }
+
+    console.log(config)
+
+    return config;
+  },
+};
