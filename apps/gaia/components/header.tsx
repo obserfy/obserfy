45382a6d4480ea444740e801e7a0e622958b8480
Chrisando Pramudhita <chrsep@protonmail.com>
2020-05-24 02:40:27 +0700

    Add mock student name.

--- apps/gaia/components/header.tsx
+++ apps/gaia/components/header.tsx
@@ -6,8 +6,8 @@ interface Props {
   userImageSrc?: string
 }
 const Header: FC<Props> = ({ userImageSrc, userName }) => (
-  <div className="bg-surface border-b ">
-    <div className="p-3 pb-2 flex items-center max-w-6xl mx-auto">
+  <>
+    <div className="p-3 pb-2 flex items-center max-w-6xl mx-auto bg-surface">
       <img
         alt="obserfy logo"
         src="/logo-transparent.png"
@@ -19,12 +19,30 @@ const Header: FC<Props> = ({ userImageSrc, userName }) => (
       </div>
       <div className="ml-3 text-sm">{userName}</div>
     </div>
-    <div className="w-full flex max-w-6xl mx-auto sticky">
-      <div className="mx-3 px-1 py-2 border-b-2 border-black text-sm">
-        Lesson Plans
+    <div className="flex p-3 max-w-6xl mx-auto bg-surface">
+      <div>
+        <img
+          alt="profile"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4LlsiK4wcMEo2fEGLfj3WNBxAIXZJSnMgSlZavkf2HZGcWzhXDyvxfZOSrJ8&s"
+          width={60}
+          height={60}
+          className="rounded-full"
+        />
+      </div>
+      <div>
+        <div className="ml-3 text-2xl">Hermione Granger</div>
+        <div className="ml-3 text-sm text-gray-700">Joyful Online</div>
       </div>
     </div>
-  </div>
+    <nav className="w-full flex max-w-6xl mx-auto sticky top-0 left-0 bg-surface border-b ">
+      <div
+        className="mx-3 px-1 py-2 border-b-2 border-black text-sm"
+        style={{ marginBottom: "-1px" }}
+      >
+        Lesson Plans
+      </div>
+    </nav>
+  </>
 )
 
 export default Header
--- apps/gaia/pages/index.tsx
+++ apps/gaia/pages/index.tsx
@@ -3,7 +3,43 @@ import React from "react"
 const IndexPage = () => {
   return (
     <main className="p-3">
-      <h1>Hello</h1>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
     </main>
   )
 }
