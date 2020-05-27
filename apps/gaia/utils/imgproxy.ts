17bdb4b5f00642f3d37475f42e93e9af554dbbec
Chrisando Pramudhita <chrsep@protonmail.com>
2020-05-28 03:02:29 +0700

    Add imgproxy integration to gaia.
    - Generate complete imgproxy URL
    - configure gaia imgproxy through .env

---
+++ apps/gaia/utils/imgproxy.ts
@@ -0,0 +1,36 @@
+import { createHmac } from "crypto"
+
+const KEY = process.env.IMGPROXY_KEY
+const SALT = process.env.IMGPROXY_SALT
+const URL = process.env.IMGPROXY_URL
+const BUCKET = process.env.IMGPROXY_BUCKET
+
+export const generateUrl = (imagePath: string, width: number, height: number) => {
+  const urlSafeBase64 = (string) => {
+    return Buffer.from(string)
+      .toString("base64")
+      .replace(/=/g, "")
+      .replace(/\+/g, "-")
+      .replace(/\//g, "_")
+  }
+
+  const hexDecode = (hex) => Buffer.from(hex, "hex")
+
+  const sign = (salt, target, secret) => {
+    const hmac = createHmac("sha256", hexDecode(secret))
+    hmac.update(hexDecode(salt))
+    hmac.update(target)
+    return urlSafeBase64(hmac.digest())
+  }
+
+  const url = `s3://${BUCKET}/${imagePath}`
+  const resizingType = "fill"
+  const gravity = "no"
+  const enlarge = 1
+  const extension = "png"
+  const encodedUrl = urlSafeBase64(url)
+  const path = `/${resizingType}/${width}/${height}/${gravity}/${enlarge}/${encodedUrl}.${extension}`
+
+  const signature = sign(SALT, path, KEY)
+  return `${URL}/${signature}${path}`
+}
