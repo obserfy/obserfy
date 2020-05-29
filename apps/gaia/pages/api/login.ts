69f1201926b0f33b4cec9539bbe7b867728cbbe7
Chrisando Pramudhita <chrsep@protonmail.com>
2020-05-29 19:03:48 +0700

    Redirect user to home when they already have a session.

--- apps/gaia/pages/api/login.ts
+++ apps/gaia/pages/api/login.ts
@@ -3,6 +3,13 @@ import auth0 from "../../utils/auth0"
 
 const login: NextApiHandler = async (req, res) => {
   try {
+    const session = await auth0.getSession(req)
+    if (session) {
+      console.log(session)
+      res.setHeader("Location", "/")
+      res.status(302).end()
+      return
+    }
     await auth0.handleLogin(req, res)
   } catch (error) {
     console.error(error)
