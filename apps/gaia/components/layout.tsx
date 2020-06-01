ba5baab2457974539f49c7c9f3041be1a2024aac
Chrisando Pramudhita <chrsep@protonmail.com>
2020-06-01 17:08:33 +0700

    Fix login notice padding.

--- apps/gaia/components/layout.tsx
+++ apps/gaia/components/layout.tsx
@@ -30,8 +30,8 @@ const Layout: FC = ({ children }) => {
           <h1 className="text-2xl m-3 leading-tight">
             Looks like your session has expired
           </h1>
-          <a href="/api/login" className="block mt-6 w-full ">
-            <Button type="button" className="w-full py-3 mx-3">
+          <a href="/api/login" className="block mt-6 w-full px-3">
+            <Button type="button" className="w-full py-3">
               Login
             </Button>
           </a>
