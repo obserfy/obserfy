e39e2cab26aae649d20abff15d3b87f827a8bff9
Chrisando Pramudhita <chrsep@protonmail.com>
2020-06-06 17:55:07 +0700

    Add deleteApi function.

--- apps/vor/frontend/src/api/useDeleteClass.ts
+++ apps/vor/frontend/src/api/useDeleteClass.ts
@@ -4,33 +4,15 @@ import {
   queryCache,
   useMutation,
 } from "react-query"
-import { navigate } from "gatsby"
-import { ApiError, BASE_URL } from "./useApi"
 import { getSchoolId } from "../hooks/schoolIdState"
+import { deleteApi } from "./fetchApi"
 
 const useDeleteClass = (
   classId: string
 ): [MutateFunction<Response, undefined>, MutationResult<Response>] => {
-  const fetchApi = async (): Promise<Response> => {
-    const result = await fetch(`${BASE_URL}/classes/${classId}`, {
-      credentials: "same-origin",
-      method: "DELETE",
-    })
+  const deleteClass = deleteApi("/classes", classId)
 
-    // Throw user to login when something gets 401
-    if (result.status === 401) {
-      await navigate("/login")
-      return result
-    }
-    if (result.status !== 200) {
-      const body: ApiError = await result.json()
-      throw Error(body?.error?.message ?? "")
-    }
-
-    return result
-  }
-
-  return useMutation<Response>(fetchApi, {
+  return useMutation<Response>(deleteClass, {
     onSuccess: async () => {
       await queryCache.refetchQueries(["classes", getSchoolId()])
     },
