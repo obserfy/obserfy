ca45e43c00af83f166d176f17f972590836f4718
Chrisando Pramudhita <chrsep@protonmail.com>
2020-06-06 18:25:36 +0700

    Remove old unneeded files.

--- apps/vor/frontend/src/arrayManipulation.ts
+++
@@ -1,6 +0,0 @@
-export function addOnlyUniqueValues<T>(currentValues: T[], newValue: T): T[] {
-  if (!currentValues.includes(newValue)) {
-    currentValues.push(newValue)
-  }
-  return currentValues
-}
