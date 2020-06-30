083b531a940aca6c56dc8a8063fbf0821d68df5f
christianto <christianto88@hotmail.com>
2020-06-30 21:31:12 +0700

    add refetch for getStudents

--- apps/vor/frontend/src/api/students/usePatchStudentApi.ts
+++ apps/vor/frontend/src/api/students/usePatchStudentApi.ts
@@ -21,7 +21,10 @@ export function usePatchStudentApi(
 
   return useMutation(patchStudent, {
     onSuccess: async () => {
-      await queryCache.refetchQueries(["student", id])
+      await Promise.all([
+        queryCache.refetchQueries(["student", id]),
+        queryCache.refetchQueries("students"),
+      ])
     },
   })
 }
