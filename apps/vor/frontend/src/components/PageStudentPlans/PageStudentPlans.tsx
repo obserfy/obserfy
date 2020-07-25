75a1621b16459de040d9cf1daca3402b99d918e0
christianto <christianto88@hotmail.com>
2020-07-25 21:13:04 +0700

    fix formatting

--- apps/vor/frontend/src/components/PageStudentPlans/PageStudentPlans.tsx
+++ apps/vor/frontend/src/components/PageStudentPlans/PageStudentPlans.tsx
@@ -121,6 +121,16 @@ export const PageStudentPlans: FC<Props> = ({ studentId, date }) => {
               >
                 {plan.area ? plan.area.name : "Other"}
               </Typography.Body>
+              <Typography.Body
+                sx={{
+                  fontSize: 1,
+                  lineHeight: 1,
+                  color: "textMediumEmphasis",
+                }}
+                mt={2}
+              >
+                Author : {plan.user ? plan.user.name : "Other"}
+              </Typography.Body>
             </Card>
           </Link>
         )
