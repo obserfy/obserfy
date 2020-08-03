ba9a9c68b4218bab2f8becc977e73e2a55b27992
Chrisando Pramudhita <chrsep@protonmail.com>
2020-08-03 12:56:29 +0700

    fix redirects not working properly.

--- apps/vor/frontend/src/pages/index.tsx
+++ apps/vor/frontend/src/pages/index.tsx
@@ -1,12 +1,12 @@
 import { PageRendererProps } from "gatsby"
-import React, { FC } from "react"
+import React, { FC, useEffect } from "react"
 import { navigate } from "../components/Link/Link"
 import { STUDENTS_URL } from "../routes"
 
-const IndexPage: FC<PageRendererProps> = ({ location }) => {
-  if (typeof window !== "undefined" && location.pathname === "/") {
+const IndexPage: FC<PageRendererProps> = () => {
+  useEffect(() => {
     navigate(STUDENTS_URL)
-  }
+  }, [])
   return <div />
 }
 
