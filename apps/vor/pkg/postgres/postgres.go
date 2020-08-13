a4eeb58973a76ed75e587ae3ae040aba6cb587bd
Chrisando Pramudhita <chrsep@protonmail.com>
2020-08-13 21:56:31 +0700

    add default value for created at column.

--- apps/vor/pkg/postgres/postgres.go
+++ apps/vor/pkg/postgres/postgres.go
@@ -218,7 +218,7 @@ type School struct {
 	Guardian       []Guardian
 	SubscriptionId uuid.UUID `pg:",type:uuid"`
 	Subscription   Subscription
-	CreatedAt      time.Time
+	CreatedAt      time.Time `pg:"default:now()"`
 }
 
 type Attendance struct {
