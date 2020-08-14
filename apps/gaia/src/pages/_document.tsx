0a032ee024a14b5197fc54be0820158d54abed54
Chrisando Pramudhita <chrsep@protonmail.com>
2020-08-14 22:25:46 +0700

    add segment to gaia.

--- apps/gaia/src/pages/_document.tsx
+++ apps/gaia/src/pages/_document.tsx
@@ -1,5 +1,18 @@
 import React from "react"
 import Document, { Html, Head, Main, NextScript } from "next/document"
+import { min } from "@segment/snippet"
+
+const { NEXT_PUBLIC_GAIA_SEGMENT_KEY = "" } = process.env
+
+const renderSnippet = () => {
+  const opts = {
+    apiKey: NEXT_PUBLIC_GAIA_SEGMENT_KEY,
+    page: true,
+    load: false,
+  }
+
+  return min(opts)
+}
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -31,6 +44,45 @@ class MyDocument extends Document {
             type="font/woff2"
             crossOrigin="anonymous"
             href="/google-fonts/s/opensans/v17/mem8YaGs126MiZpBA-UFVZ0b.woff2"
+          />
+          {/* eslint-disable-next-line react/no-danger */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+window.segmentSnippetLoaded = false
+window.segmentSnippetLoading = false
+
+window.segmentSnippetLoader = function (callback) {
+  if (!window.segmentSnippetLoaded && !window.segmentSnippetLoading) {
+    window.segmentSnippetLoading = true
+
+    function loader() {
+      window.analytics.load(${NEXT_PUBLIC_GAIA_SEGMENT_KEY})
+      window.segmentSnippetLoading = false
+      window.segmentSnippetLoaded = true
+      if (callback) {
+        callback()
+      }
+    }
+
+    setTimeout(function () {
+      "requestIdleCallback" in window
+        ? requestIdleCallback(function () {
+            loader()
+          })
+        : loader()
+    }, 1000)
+  }
+}
+window.addEventListener(
+  "scroll",
+  function () {
+    window.segmentSnippetLoader()
+  },
+  { once: true }
+)
+${renderSnippet()}`,
+            }}
           />
         </Head>
         <body>
