4813584fe56a6f3df0f8fea1049507062946283b
Chrisando Pramudhita <chrsep@protonmail.com>
2020-08-17 21:37:12 +0700

    fix segment error.

--- apps/gaia/src/pages/_document.tsx
+++ apps/gaia/src/pages/_document.tsx
@@ -1,5 +1,18 @@
 import React from "react"
 import Document, { Html, Head, Main, NextScript } from "next/document"
+import { min } from "@segment/snippet"
+
+const { NEXT_PUBLIC_GAIA_SEGMENT_KEY = "" } = process.env
+
+const renderSnippet = () => {
+  const opts = {
+    apiKey: NEXT_PUBLIC_GAIA_SEGMENT_KEY,
+    page: true,
+    load: false,
+  }
+
+  return min(opts)
+}
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -32,6 +45,8 @@ class MyDocument extends Document {
             crossOrigin="anonymous"
             href="/google-fonts/s/opensans/v17/mem8YaGs126MiZpBA-UFVZ0b.woff2"
           />
+          {/* eslint-disable-next-line react/no-danger */}
+          <script dangerouslySetInnerHTML={{ __html: renderSnippet() }} />
         </Head>
         <body>
           <Main />
