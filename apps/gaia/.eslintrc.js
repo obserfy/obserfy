c53e4235abf97cc9bca6798960c50468dfe8e0cb
Chrisando Pramudhita <chrsep@protonmail.com>
2020-08-17 21:56:28 +0700

    fix eslint errors.

--- apps/gaia/.eslintrc.js
+++ apps/gaia/.eslintrc.js
@@ -33,6 +33,7 @@ module.exports = {
     "react/prop-types": 0,
     "react/style-prop-object": 0,
     "react/jsx-props-no-spreading": 0,
+    "jsx-a11y/anchor-is-valid": 0,
     // Recommended for immer.
     "no-param-reassign": [
       "error",
--- apps/gaia/src/components/Navbar/Navbar.tsx
+++ apps/gaia/src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from "react"
+import React, { FC } from "react"
 import { useRouter } from "next/router"
 import Link from "next/link"
 
@@ -6,34 +6,31 @@ interface Props {
   childId: string
 }
 const Navbar: FC<Props> = ({ childId }) => {
-  const [active, setActive] = useState("lesson-plan")
   const router = useRouter()
-  if (router.pathname !== "/") {
-    setActive(router.pathname.slice(1, router.pathname.length))
-  }
+
   return (
     <div className="sticky top-0 bg-surface border-b">
-      <nav className="w-full flex max-w-3xl mx-auto pt-3">
+      <nav className="w-full flex max-w-3xl mx-auto pt-3 pl-1">
         <ul className="flex">
-          <li className="-mb-px mr-1" onClick={() => setActive("lesson-plan")}>
-            <Link href="/">
+          <li className="-mb-px mr-1">
+            <Link href={`/?childId=${childId}`}>
               <a
                 className={`${
-                  active === "lesson-plan" ? "border-b border-black" : ""
-                } bg-white inline-block py-2 px-4 text-blue-700 font-semibold ${
-                  active === "lesson-plan" ? "border-b border-black" : ""
-                }`}
+                  router.pathname === "/" ? "border-b-2 border-black" : ""
+                } bg-white inline-block p-2 text-sm`}
               >
                 Lesson Plan
               </a>
             </Link>
           </li>
-          <li className="mr-1" onClick={() => setActive("gallery")}>
+          <li className="mr-1">
             <Link href={`/gallery?childId=${childId}`}>
               <a
                 className={`${
-                  active === "gallery" ? "border-b border-black" : ""
-                } bg-white inline-block py-2 px-4 text-blue-700 font-semibold`}
+                  router.pathname === "/gallery"
+                    ? "border-b-2 border-black"
+                    : ""
+                } bg-white inline-block p-2 text-sm`}
               >
                 Gallery
               </a>
