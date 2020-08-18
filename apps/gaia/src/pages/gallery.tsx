d50d79de44319bf1018fc6e73deeb1bf77bedc52
Chrisando Pramudhita <chrsep@protonmail.com>
2020-08-18 15:26:19 +0700

    add no image illustrations to gaia.

--- apps/gaia/src/pages/gallery.tsx
+++ apps/gaia/src/pages/gallery.tsx
@@ -1,7 +1,9 @@
-import React from "react"
+import React, { FC } from "react"
 import Head from "next/head"
+import Img from "react-optimized-image"
 import useGetChildImages from "../hooks/useGetChildImages"
 import { useQueryString } from "../hooks/useQueryString"
+import NoImagesIllustration from "../images/no-images-illustration.svg"
 
 const GalleryPage = () => {
   const childId = useQueryString("childId")
@@ -13,7 +15,7 @@ const GalleryPage = () => {
       </Head>
       <div className="max-w-3xl mx-auto flex items-center">
         <div className="flex mx-auto flex-wrap">
-          {childImages.data?.map((img) => (
+          {childImages.data.map((img) => (
             <div
               key={img.image_id}
               style={{ maxWidth: "33.3333%" }}
@@ -27,9 +29,31 @@ const GalleryPage = () => {
               />
             </div>
           ))}
+          {childImages.data.length === 0 && (
+            <EmptyGalleryIllustration loading={childImages.isLoading} />
+          )}
         </div>
       </div>
     </>
+  )
+}
+
+const EmptyGalleryIllustration: FC<{ loading: boolean }> = ({ loading }) => {
+  return (
+    <div
+      className={`flex flex-col items-center py-16 ${
+        loading && "opacity-50"
+      } transition-opacity duration-200 max-w-3xl mx-auto`}
+    >
+      <Img src={NoImagesIllustration} className="w-64 md:w-1/2 mb-3" />
+      <h6
+        className={`text-xl mx-4 text-center ${
+          loading && "opacity-0"
+        } transition-opacity duration-200 font-bold`}
+      >
+        No images yet
+      </h6>
+    </div>
   )
 }
 
