74a308738a0da118db6b8549347ad3c3bb0ae39b
Chrisando Pramudhita <chrsep@protonmail.com>
2020-09-09 00:44:51 +0700

    fix broken page.

--- apps/vor/frontend/src/pages/dashboard/admin/guardians/profile.tsx
+++ apps/vor/frontend/src/pages/dashboard/admin/guardians/profile.tsx
@@ -3,7 +3,7 @@ import { useQueryString } from "../../../../hooks/useQueryString"
 import PageGuardianProfile from "../../../../components/PageGuardianProfile/PageGuardianProfile"
 
 const GuardianProfile = () => {
-  const id = useQueryString("studentId")
+  const id = useQueryString("id")
 
   return <PageGuardianProfile guardianId={id} />
 }
