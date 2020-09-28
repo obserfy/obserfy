cc0f607069274689ac333280bac9153d98030d0b
Chrisando Pramudhita <chrsep@protonmail.com>
2020-09-28 20:47:37 +0700

    disable scroll hack.
    We used to use a hack to somehow preserve scroll position on certain page navigation (such as top level bottom nav link clicks and back button).
    
    This hack breaks apart one day because of some changes inside gatsby, we are now temporarily disabling it.

--- apps/vor/frontend/gatsby-browser.js
+++ apps/vor/frontend/gatsby-browser.js
@@ -7,37 +7,42 @@
 // You can delete this file if you're not using it
 import "./src/global.css"
 
+// Disabled because it currently breaks due to gatsby's changes.
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export const shouldUpdateScroll = ({
-  prevRouterProps: { location },
-  routerProps,
-  getSavedScrollPosition,
-}) => {
-  // =============== What is all of this??? ==============================
-  // This is a simple hack to preserve scroll position for some navigation links
-  // (such as link for going back and bottom navigation link).
-
-  // Get scroll position of the current page the user in
-  const lastPosition = getSavedScrollPosition(location)
-  // undocumented internals.
-  window.sessionStorage.setItem(
-    `@@scroll|${location.pathname}`,
-    JSON.stringify(lastPosition)
-  )
-
-  // If state.preserveScroll is true, we'll use the last known
-  // scroll position of the given pathname, and scroll to it.
-  if (routerProps.location.state?.preserveScroll) {
-    const currentPosition = window.sessionStorage.getItem(
-      `@@scroll|${routerProps.location.pathname}`
-    )
-    const position = JSON.parse(currentPosition)
-    window.scrollTo(...(position || [0, 0]))
-    return false
-  }
-
-  return true
-}
+// export const shouldUpdateScroll = ({
+//   prevRouterProps: { location },
+//   routerProps,
+//   getSavedScrollPosition,
+// }) => {
+//   // =============== What is all of this??? ==============================
+//   // This is a simple hack to preserve scroll position for some navigation links
+//   // (such as link for going back and bottom navigation link).
+//
+//   // Get scroll position of the current page the user in
+//   const lastPosition = getSavedScrollPosition(location)
+//   // undocumented internals.
+//   window.sessionStorage.setItem(
+//     `@@scroll|${location.pathname}`,
+//     JSON.stringify(lastPosition)
+//   )
+//   console.log(`@@scroll|${location.pathname}`)
+//
+//   // If state.preserveScroll is true, we'll use the last known
+//   // scroll position of the given pathname, and scroll to it.
+//   if (routerProps.location.state?.preserveScroll) {
+//     const currentPosition = window.sessionStorage.getItem(
+//       `@@scroll|${routerProps.location.pathname}`
+//     )
+//     const position = JSON.parse(currentPosition)
+//     window.scrollTo(...(position || [0, 0]))
+//
+//     console.log(currentPosition)
+//     console.log(routerProps.location.pathname)
+//     return false
+//   }
+//
+//   return true
+// }
 
 export const onServiceWorkerUpdateReady = () => {
   if (window.updateAvailable) {
