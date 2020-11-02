abccd928ce89f4c174da8d970e0a1b658be15f63
Chrisando Pramudhita <chrsep@protonmail.com>
2020-11-03 00:43:53 +0700

    add empty timeline placeholder.

--- apps/gaia/src/pages/index.tsx
+++ apps/gaia/src/pages/index.tsx
@@ -7,17 +7,18 @@ import { useQueryString } from "../hooks/useQueryString"
 import { GetChildTimelineResponse } from "./api/children/[childId]/timeline"
 import CalendarIcon from "../icons/calendar.svg"
 import EditIcon from "../icons/edit.svg"
+import EmptyTimeline from "../images/undraw_Preparation_re_t0ce.svg"
 
 const IndexPage = () => {
   const childId = useQueryString("childId")
-  const { data: timeline } = useGetTimeline(childId)
+  const { data: timeline, isLoading } = useGetTimeline(childId)
 
   return (
     <div>
       <Head>
         <title>Obserfy for Parents</title>
       </Head>
-      <div className="max-w-3xl mx-auto flex items-center">
+      <div className="max-w-3xl mx-auto">
         <div className="border-l ml-8 pt-3">
           {timeline?.map(({ date, observations }) => (
             <DateItem key={date} date={date} observations={observations} />
@@ -27,6 +28,9 @@ const IndexPage = () => {
             <div className="w-4 h-4  mx-1 bg-white rounded-full border " />
           </div>
         </div>
+        {(timeline?.length ?? 0) === 0 && (
+          <EmptyPlansIllustration loading={isLoading} />
+        )}
       </div>
     </div>
   )
@@ -60,26 +64,24 @@ const DateItem: FC<{
   </div>
 )
 
-// const EmptyPlansIllustration: FC<{ loading: boolean; date: Dayjs }> = ({
-//   loading,
-//   date,
-// }) => {
-//   return (
-//     <div
-//       className={`flex flex-col items-center py-16 ${
-//         loading && "opacity-50"
-//       } transition-opacity duration-200 max-w-3xl mx-auto`}
-//     >
-//       <Img src={NoPlanIllustration} className="w-64 md:w-1/2 mb-3" />
-//       <h5
-//         className={`text-xl mx-4 text-center ${
-//           loading && "opacity-0"
-//         } transition-opacity duration-200 font-bold`}
-//       >
-//         No plans for {date.format("MMMM D")}
-//       </h5>
-//     </div>
-//   )
-// }
+const EmptyPlansIllustration: FC<{ loading: boolean }> = ({ loading }) => {
+  return (
+    <div
+      className={`flex flex-col items-center pt-16 ${
+        loading && "opacity-50"
+      } transition-opacity duration-200 max-w-3xl mx-auto`}
+    >
+      <Img src={EmptyTimeline} className="w-64 md:w-1/2 mb-4" />
+      <h5
+        className={`text-xl mx-4 text-center ${
+          loading && "opacity-0"
+        } transition-opacity duration-200 font-bold`}
+      >
+        Timeline is empty right now. <br />
+        Check again later
+      </h5>
+    </div>
+  )
+}
 
 export default IndexPage
