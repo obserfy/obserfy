f002348827c679bf9e9d5227460b6da974055de3
Chrisando Pramudhita <chrsep@protonmail.com>
2020-11-06 15:00:23 +0700

    remove unused deps.

--- apps/freyr/gatsby-config.js
+++ apps/freyr/gatsby-config.js
@@ -26,21 +26,10 @@ module.exports = {
         name: `Obserfy`,
         short_name: `Obserfy`,
         start_url: `/`,
-        background_color: `#121212`,
-        theme_color: `#121212`,
+        background_color: `#ffffff`,
+        theme_color: `#ffffff`,
         display: `standalone`,
         icon: `src/images/logo-standalone.svg`, // This path is relative to the root of the site.
-      },
-    },
-    {
-      resolve: `gatsby-plugin-purgecss`,
-      options: {
-        printRejected: true, // Print removed selectors and processed file names
-        // develop: true, // Enable while using `gatsby develop`
-        tailwind: true, // Enable tailwindcss support
-        // whitelist: ['whitelist'], // Don't remove this selector
-        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
-        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       },
     },
     {
@@ -62,30 +51,10 @@ module.exports = {
       resolve: "gatsby-plugin-webpack-bundle-analyser-v2",
       options: {
         analyzerPort: 3000,
+        disable: true,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
     `gatsby-plugin-remove-trailing-slashes`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: "UA-158765938-2",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        // exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Set Variation ID. 0 for original 1,2,3....
-        variationId: "0",
-      },
-    },
     {
       resolve: `gatsby-theme-i18n`,
       options: {
