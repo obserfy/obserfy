914ff65cdeb1b9982c5467b5543948a963c1b57a
Tiffany <tiff.wijaya@gmail.com>
2020-11-20 21:14:19 +0700

    update pnp file

--- apps/gaia/src/hooks/api/useGetImageObservation.ts
+++ apps/gaia/src/hooks/api/useGetImageObservation.ts
@@ -3,7 +3,9 @@ import { GetChildObservationByImages } from "../../pages/api/image/[imageId]"
 import { getApi } from "../../apiHelpers"
 
 const useGetObservationsByImage = (imageId: string) => {
-  const getObservationsByImage = getApi<GetChildObservationByImages>(`/image/${imageId}`)
+  const getObservationsByImage = getApi<GetChildObservationByImages>(
+    `/image/${imageId}`
+  )
   return useQuery(["image-observations", imageId], getObservationsByImage)
 }
 
