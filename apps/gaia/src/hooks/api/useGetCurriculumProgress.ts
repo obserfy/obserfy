30c25873556f4279be91b22e67271ada5c5cfa66
Chrisando Pramudhita <chrsep@protonmail.com>
2020-11-22 19:34:15 +0700

    add ui for curriculum progress.

--- apps/gaia/src/hooks/api/useGetCurriculumProgress.ts
+++ apps/gaia/src/hooks/api/useGetCurriculumProgress.ts
@@ -1,13 +1,21 @@
 import { useQuery } from "react-query"
 import { getApi } from "./apiHelpers"
 import { GetChildProgressResponse } from "../../pages/api/children/[childId]/progress"
+import { Area } from "../../domain"
 
-const useGetCurriculumProgress = (childId: string) => {
+const useGetCurriculumProgress = (
+  childId: string,
+  onSuccess: (data: Area[]) => void = () => {}
+) => {
   const getCurriculumProgress = getApi<GetChildProgressResponse>(
     `/children/${childId}/progress`
   )
 
-  return useQuery(["child", childId, "progress"], getCurriculumProgress)
+  return useQuery(["child", childId, "progress"], getCurriculumProgress, {
+    onSuccess: (data) => {
+      onSuccess(data)
+    },
+  })
 }
 
 export default useGetCurriculumProgress
