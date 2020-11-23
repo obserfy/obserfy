a341cf63600a12c49cc7509dafb2f4d58839578c
Chrisando Pramudhita <chrsep@protonmail.com>
2020-11-23 18:44:00 +0700

    change note placeholder text.

--- apps/vor/frontend/src/components/PageStudentProfile/PageStudentProfile.tsx
+++ apps/vor/frontend/src/components/PageStudentProfile/PageStudentProfile.tsx
@@ -90,7 +90,7 @@ export const PageStudentProfile: FC<Props> = ({ studentId }) => {
         />
         <NotesDataBox
           value={data?.note}
-          key={`doe${data?.note}`}
+          key={`note${data?.note}`}
           studentId={studentId}
         />
       </Card>
@@ -99,21 +99,14 @@ export const PageStudentProfile: FC<Props> = ({ studentId }) => {
         <Flex sx={{ alignItems: "flex-start" }}>
           <Box px={3} py={3}>
             <Typography.Body
-              sx={{
-                fontSize: 0,
-                lineHeight: 1,
-              }}
+              sx={{ lineHeight: 1 }}
               mb={2}
               color="textMediumEmphasis"
             >
               <Trans>Classes</Trans>
             </Typography.Body>
             {data?.classes?.length === 0 && (
-              <Typography.Body
-                sx={{
-                  lineHeight: 1,
-                }}
-              >
+              <Typography.Body sx={{ lineHeight: 1 }}>
                 <Trans>Not Set</Trans>
               </Typography.Body>
             )}
@@ -157,21 +150,19 @@ export const PageStudentProfile: FC<Props> = ({ studentId }) => {
                 <Trans>Not Set</Trans>
               </Typography.Body>
             )}
-            {data?.guardians?.map(({ id, email, name }) => {
-              return (
-                <Box py={3} key={id}>
-                  <Typography.Body sx={{ lineHeight: 1 }} mb={2}>
-                    {name}
-                  </Typography.Body>
-                  <Typography.Body
-                    sx={{ lineHeight: 1, fontSize: 1 }}
-                    color="textMediumEmphasis"
-                  >
-                    <Trans id={email || t`No email`} />
-                  </Typography.Body>
-                </Box>
-              )
-            })}
+            {data?.guardians?.map(({ id, email, name }) => (
+              <Box py={3} key={id}>
+                <Typography.Body sx={{ lineHeight: 1 }} mb={2}>
+                  {name}
+                </Typography.Body>
+                <Typography.Body
+                  sx={{ lineHeight: 1, fontSize: 1 }}
+                  color="textMediumEmphasis"
+                >
+                  <Trans id={email || t`No email`} />
+                </Typography.Body>
+              </Box>
+            ))}
           </Box>
           <Link
             to={EDIT_GUARDIANS_URL(studentId)}
@@ -354,11 +345,12 @@ const NotesDataBox: FC<{ value?: string; studentId: string }> = ({
     await mutate({ note })
     setShowEditDialog(false)
   }
+
   return (
     <Fragment>
       <DataBox
         label={t`Notes`}
-        value={note || t`Not Set`}
+        value={note || "-"}
         onEditClick={() => setShowEditDialog(true)}
       />
       {showEditDialog && (
