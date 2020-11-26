a6b70b5a43f3fae16b94c4d19d15c57d03f12818
Chrisando Pramudhita <chrsep@protonmail.com>
2020-11-26 16:29:05 +0700

    use sentry on server.

--- apps/gaia/src/pages/api/image/index.ts
+++ apps/gaia/src/pages/api/image/index.ts
@@ -2,9 +2,9 @@ import { NextApiRequest, NextApiResponse } from "next"
 import { v4 as uuidv4 } from "uuid"
 import { File, IncomingForm } from "formidable-serverless"
 import { promises as fs } from "fs"
-import auth0 from "../../../utils/auth0"
 import { insertImage } from "../../../db/queries"
 import uploadFile from "../../../utils/minio"
+import { protectedApiRoute } from "../../../utils/rest"
 
 export const config = {
   api: {
@@ -51,19 +51,12 @@ const handlePost = async (res: NextApiResponse, req: NextApiRequest) => {
   res.status(200).end()
 }
 
-const imageHandler = auth0.requireAuthentication(
-  async (req: NextApiRequest, res: NextApiResponse) => {
-    try {
-      if (req.method === "POST") {
-        await handlePost(res, req)
-      } else {
-        res.status(405)
-      }
-    } catch (error) {
-      console.error(error)
-      res.status(error.status || 500).end(error.message)
-    }
+const imageHandler = protectedApiRoute(async (req, res) => {
+  if (req.method === "POST") {
+    await handlePost(res, req)
+  } else {
+    res.status(405)
   }
-)
+})
 
 export default imageHandler
