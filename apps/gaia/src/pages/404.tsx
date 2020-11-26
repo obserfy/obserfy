27fe48fb5a21bfce6649fff7da1fc00294af57ab
Chrisando Pramudhita <chrsep@protonmail.com>
2020-11-26 17:50:15 +0700

    detect 404.

--- apps/gaia/src/pages/404.tsx
+++ apps/gaia/src/pages/404.tsx
@@ -1,7 +1,11 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Error from "next/error"
 
 export default function NotFound() {
+  useEffect(() => {
+    mixpanel.track("404 visited")
+  }, [])
+
   // Opinionated: do not record an exception in Sentry for 404
   return <Error statusCode={404} />
 }
