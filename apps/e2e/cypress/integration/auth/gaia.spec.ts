7fef153a057a9846881f608fbbdc5eae77136195
Chrisando Pramudhita <chrsep@protonmail.com>
2020-11-27 21:30:31 +0700

    increase time.

--- apps/e2e/cypress/integration/auth/gaia.spec.ts
+++ apps/e2e/cypress/integration/auth/gaia.spec.ts
@@ -7,13 +7,13 @@ describe("test gaia authentication", () => {
 
   it("should be redirected to login when logged out", () => {
     cy.visitGaia("/")
-    cy.wait(200)
+    cy.wait(500)
     cy.url().should("include", "auth0.com")
   })
 
   it("should show info when user have no data", () => {
     cy.visitGaia("/")
-    cy.wait(300)
+    cy.wait(500)
     cy.get("#username").type(Cypress.env("GAIA_USERNAME"))
     cy.get("#password").type(Cypress.env("GAIA_PASSWORD"))
     cy.contains("Continue").click()
