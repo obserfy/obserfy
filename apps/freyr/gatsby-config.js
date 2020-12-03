f5f608f3f075422be63a6752c7a5e13e309c00c5
Chrisando Pramudhita <chrsep@protonmail.com>
2020-12-03 14:49:54 +0700

    ref(freyr): migrate to gatsby's new image API.

--- apps/freyr/gatsby-config.js
+++ apps/freyr/gatsby-config.js
@@ -17,6 +17,7 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    `gatsby-plugin-image`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-netlify`,
