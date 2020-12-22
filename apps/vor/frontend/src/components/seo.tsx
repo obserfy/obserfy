dd555f774a679f3e1f010ea338430a03fa2e0faa
Chrisando Pramudhita <chrsep@protonmail.com>
2020-12-23 00:36:04 +0700

    implement i18n for vor seo component.

--- apps/vor/frontend/src/components/seo.tsx
+++ apps/vor/frontend/src/components/seo.tsx
@@ -1,15 +1,72 @@
+import { useLingui } from "@lingui/react"
+import { graphql, useStaticQuery } from "gatsby"
 import React, { FC } from "react"
 import { Helmet } from "react-helmet"
 
 interface Props {
+  description?: string
+  meta?: JSX.IntrinsicElements["meta"][]
   title: string
 }
-const SEO: FC<Props> = ({ title }) => {
-  const titleTemplate = `Obserfy | %s`
+const SEO: FC<Props> = ({ title, meta = [], description = `` }) => {
+  const { i18n } = useLingui()
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            author
+          }
+        }
+      }
+    `
+  )
+
+  const metaDescription = description || site.siteMetadata.description
 
   return (
-    <Helmet title={title} titleTemplate={titleTemplate}>
-      <html lang="en" />
+    <Helmet
+      htmlAttributes={{ lang: i18n.locale }}
+      title={i18n._(title)}
+      titleTemplate={`%s - ${site.siteMetadata.title}`}
+      meta={[
+        {
+          name: `description`,
+          content: i18n._(metaDescription),
+        },
+        {
+          property: `og:title`,
+          content: i18n._(title),
+        },
+        {
+          property: `og:description`,
+          content: i18n._(metaDescription),
+        },
+        {
+          property: `og:type`,
+          content: `website`,
+        },
+        {
+          name: `twitter:card`,
+          content: `summary`,
+        },
+        {
+          name: `twitter:creator`,
+          content: site.siteMetadata.author,
+        },
+        {
+          name: `twitter:title`,
+          content: i18n._(title),
+        },
+        {
+          name: `twitter:description`,
+          content: i18n._(metaDescription),
+        },
+        ...meta,
+      ]}
+    >
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover"
