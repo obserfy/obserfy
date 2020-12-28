253d98a23a7ef90b80b61a7b4e694f9ff4163fd7
Chrisando Pramudhita <chrsep@protonmail.com>
2020-12-29 00:17:07 +0700

    feat: add export endpoint.

---
+++ apps/vor/pkg/exports/exports.go
@@ -0,0 +1,90 @@
+package exports
+
+import (
+	"github.com/chrsep/vor/pkg/auth"
+	"github.com/chrsep/vor/pkg/domain"
+	"github.com/chrsep/vor/pkg/rest"
+	"github.com/go-chi/chi"
+	"net/http"
+)
+
+type Store interface {
+	GetObservations(schoolId string, studentId string, search string, startDate string, endDate string) ([]domain.Observation, error)
+	CheckPermissions(schoolId string, userId string) (bool, error)
+}
+
+func NewRouter(s rest.Server, store Store) *chi.Mux {
+	r := chi.NewRouter()
+	r.Route("/{schoolId}", func(r chi.Router) {
+		observationAuthMiddleware(s, store)
+		r.Method("GET", "/observations", exportObservations(s, store))
+	})
+	return r
+}
+
+func observationAuthMiddleware(s rest.Server, store Store) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return s.NewHandler(func(w http.ResponseWriter, r *http.Request) *rest.Error {
+			schoolId := chi.URLParam(r, "schoolId")
+
+			// Verify user access to the school
+			session, ok := auth.GetSessionFromCtx(r.Context())
+			if !ok {
+				return auth.NewGetSessionError()
+			}
+			userHasAccess, err := store.CheckPermissions(schoolId, session.UserId)
+			if err != nil {
+				return &rest.Error{
+					Code:    http.StatusInternalServerError,
+					Message: "Internal Server Error",
+					Error:   err,
+				}
+
+			}
+			// Check if user is related to the school
+			if !userHasAccess {
+				return &rest.Error{
+					Code:    http.StatusNotFound,
+					Message: "Observation not found",
+					Error:   err,
+				}
+			}
+
+			next.ServeHTTP(w, r)
+			return nil
+		})
+	}
+}
+
+func exportObservations(s rest.Server, store Store) http.Handler {
+	type response struct {
+		ShortDesc string `csv:"shortDesc"`
+		LongDesc  string `csv:"longDesc"`
+	}
+	return s.NewHandler(func(w http.ResponseWriter, r *http.Request) *rest.Error {
+		queries := r.URL.Query()
+		schoolId := chi.URLParam(r, "schoolId")
+		studentId := queries.Get("studentId")
+		searchQuery := queries.Get("search")
+		startDateQuery := queries.Get("startDate")
+		endDateQuery := queries.Get("endDate")
+
+		observations, err := store.GetObservations(schoolId, studentId, searchQuery, startDateQuery, endDateQuery)
+		if err != nil {
+			return rest.NewInternalServerError(err, "failed to get observations")
+		}
+
+		body := make([]response, 0)
+		for _, observation := range observations {
+			body = append(body, response{
+				LongDesc:  observation.LongDesc,
+				ShortDesc: observation.ShortDesc,
+			})
+		}
+		if err := rest.WriteCsv(w, body); err != nil {
+			return rest.NewWriteCsvError(err)
+		}
+
+		return nil
+	})
+}
