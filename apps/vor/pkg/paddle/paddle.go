2e2b712be280614f90d28a497073be63f5a27216
Chrisando Pramudhita <chrsep@protonmail.com>
2021-01-01 02:01:39 +0700

    improve paddle billing service.

--- apps/vor/pkg/paddle/paddle.go
+++ apps/vor/pkg/paddle/paddle.go
@@ -9,18 +9,36 @@ import (
 	"strconv"
 )
 
-func UpdateSubscriptionUserCount(l zap.Logger, subscriptionId string, quantity int) error {
-	updateUserApi := "https://vendors.paddle.com/api/2.0/subscription/users/update"
-	vendorId, apiKey, err := getConfig()
-	if err != nil {
-		l.Warn("billing disabled, " + err.Error())
-		return nil
+// NewBillingService creates a new paddle.BillingService
+func NewBillingService(logger *zap.Logger) (BillingService, error) {
+	vendorId := os.Getenv("PADDLE_VENDOR_ID")
+	apiKey := os.Getenv("PADDLE_API_KEY")
+
+	if vendorId == "" || apiKey == "" {
+		return BillingService{}, richErrors.New("empty paddle credentials")
 	}
 
-	// TODO: Update next bill amount
-	_, err = http.PostForm(updateUserApi, url.Values{
-		"vendor_id":        {vendorId},
-		"vendor_auth_code": {apiKey},
+	return BillingService{
+		vendorId: vendorId,
+		apiKey:   apiKey,
+		logger:   logger,
+	}, nil
+}
+
+// BillingService implements domain.BillingService that's powered by paddle.
+type BillingService struct {
+	vendorId string
+	apiKey   string
+	logger   *zap.Logger
+}
+
+// UpdateSubscriptionQty changes the subscription qty saved on paddle.
+func (s BillingService) UpdateSubscriptionQty(subscriptionId string, quantity int) error {
+	updateUserApi := "https://vendors.paddle.com/api/2.0/subscription/users/update"
+
+	_, err := http.PostForm(updateUserApi, url.Values{
+		"vendor_id":        {s.vendorId},
+		"vendor_auth_code": {s.apiKey},
 		"subscription_id":  {subscriptionId},
 		"quantity":         {strconv.Itoa(quantity)},
 	})
@@ -29,14 +47,4 @@ func UpdateSubscriptionUserCount(l zap.Logger, subscriptionId string, quantity i
 	}
 
 	return nil
-}
-
-func getConfig() (string, string, error) {
-	vendorId := os.Getenv("PADDLE_VENDOR_ID")
-	apiKey := os.Getenv("PADDLE_API_KEY")
-	if vendorId == "" || apiKey == "" {
-		return "", "", richErrors.New("empty paddle creds")
-	}
-
-	return vendorId, apiKey, nil
 }
