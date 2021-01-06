9dd535a31106d6d7b5e6b798cf4fb918c1525fe9
Chrisando Pramudhita <chrsep@protonmail.com>
2021-01-07 06:35:30 +0700

    improve sidebar. improve introduction.

--- apps/freyr/src/templates/docs.js
+++ apps/freyr/src/templates/docs.js
@@ -11,29 +11,44 @@ const Docs = ({ data }) => (
 
     <div className="max-w-7xl mx-auto relative">
       <div className="flex px-3">
-        <div className={"w-60 prose prose-sm"}>
+        <div className={"w-60 prose prose-sm sticky top-0"}>
           <h3>
             <Trans>Documentations</Trans>
           </h3>
-          <SidebarHeader text={t`Quickstart`} />
-          <SidebarEntry text={t`Introductions`} to={"/docs"}/>
+          <SidebarHeader text={t`Concepts`} />
+          <SidebarEntry text={t`Introductions`} to={"/docs"} />
+
+          <SidebarHeader text={t`School`} />
           <SidebarEntry text={t`Inviting your team`} />
-          <SidebarEntry text={t`Giving access to parents`} />
+          <SidebarEntry text={t`Giving parents access`} />
+          <SidebarEntry text={t`Curriculum`} />
+          <SidebarEntry text={t`Classes`} />
+          <SidebarEntry text={t`Pricing & Billing`} />
+
+          <SidebarHeader text={t`Students`} />
+          <SidebarEntry text={t`Student profiles`} />
+          <SidebarEntry text={t`Guardian profiles`} />
+          <SidebarEntry text={t`Sharing data to guardians`} />
 
           <SidebarHeader text={t`Observations`} />
           <SidebarEntry text={t`Writing observations`} />
-          <SidebarEntry text={t`Sharing with parents`} />
+          <SidebarEntry text={t`Collaborating with parents`} />
 
           <SidebarHeader text={t`Lesson Plans`} />
           <SidebarEntry text={t`Creating Lesson plan`} />
           <SidebarEntry text={t`Repetitions`} />
           <SidebarEntry text={t`Sharing with parents`} />
 
-          <SidebarHeader text={t`Billing`} />
+          {/*<SidebarHeader text={t`Billing`} />*/}
+
+          <SidebarHeader text={t`Support`} />
+          <SidebarEntry text={t`Giving feedbacks`} />
+          <SidebarEntry text={t`Contacting us`} />
 
           <SidebarHeader text={t`Open source`} />
-          <SidebarEntry text={t`Open source by default`} />
+          <SidebarEntry text={t`Open-source by default`} />
           <SidebarEntry text={t`Contributing`} />
+          <SidebarEntry text={t`Self-hosting Obserfy`} />
         </div>
 
         <main className="prose px-5">
