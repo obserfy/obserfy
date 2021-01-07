be67fb0d5968a260aa74d554abc17e5ea39e53f9
Chrisando Pramudhita <chrsep@protonmail.com>
2021-01-07 23:44:53 +0700

    ref: extract duplicated lines to components.

--- apps/freyr/src/components/DocsHeader.tsx
+++ apps/freyr/src/components/DocsHeader.tsx
@@ -3,7 +3,8 @@ import React, { FC, useState } from "react"
 import CloseIcon from "../icons/close.svg"
 import MenuIcon from "../icons/menu.svg"
 import Button from "./Button/Button"
-import { Link } from "./Link"
+import DocsLogo from "./DocsLogo"
+import DocsSidebar from "./DocsSidebar"
 
 const DocsHeader: FC = () => {
   const [showSidebar, setShowSidebar] = useState(false)
@@ -76,17 +77,16 @@ const Sidebar: FC<{ show: boolean; onClose: () => void }> = ({
 }) => {
   return (
     <div
-      className={`fixed shadow w-4/5 h-full left-0 ${
+      className={`fixed shadow-lg border-r w-4/5 h-full left-0 ${
         show ? "" : "-left-full"
-      } top-0 bottom-0 bg-white z-50 transition-all duration-200 ease-in-out md:hidden border-l`}
+      } top-0 bottom-0 bg-white z-50 transition-all duration-200 ease-in-out md:hidden border-l overflow-auto`}
     >
-      <div className="m-2 mb-8 items-start">
+      <div className="m-2 mb-8">
         <div className="flex items-center">
-          <div className="prose pl-6 opacity-60">
-            <h2 className="m-0">
-              <Trans>Links</Trans>
-            </h2>
+          <div className="px-3">
+            <DocsLogo />
           </div>
+
           <Button
             secondary
             className="flex-shrink-0 mx-1 px-3 ml-auto"
@@ -95,59 +95,12 @@ const Sidebar: FC<{ show: boolean; onClose: () => void }> = ({
             <img alt="open menu" src={CloseIcon} />
           </Button>
         </div>
-        <div className="prose flex flex-col ml-6">
-          <h3 className="my-2">
-            <Link
-              to="/docs"
-              className="no-underline font-bold hover:text-green-700 transition-colors"
-            >
-              Documentation
-            </Link>
-          </h3>
-          <h3 className="my-2">
-            <Link
-              to="/pricing"
-              className="no-underline font-bold hover:text-green-700 transition-colors"
-            >
-              <Trans>Pricing</Trans>
-            </Link>
-          </h3>
-          <h3 className="my-2">
-            <a
-              href="https://feedback.obserfy.com"
-              className="no-underline font-bold hover:text-green-700 transition-colors"
-            >
-              <Trans>Roadmap</Trans>
-            </a>
-          </h3>
-          <h3 className="my-2">
-            <a
-              href="https://feedback.obserfy.com/changelog"
-              className="no-underline font-bold hover:text-green-700 transition-colors"
-            >
-              <Trans>What&apos;s New</Trans>
-            </a>
-          </h3>
-          {/* <h2> */}
-          {/*  <Link */}
-          {/*    to="/blog" */}
-          {/*    className="no-underline font-bold hover:text-green-700 transition-colors" */}
-          {/*  > */}
-          {/*    Blog */}
-          {/*  </Link> */}
-          {/* </h2> */}
-          <h3 className="my-2">
-            <Link
-              to="/contact"
-              className="no-underline font-bold hover:text-green-700 transition-colors"
-            >
-              <Trans>Contact Us</Trans>
-            </Link>
-          </h3>
+
+        <div className="p-3">
+          <DocsSidebar />
         </div>
       </div>
     </div>
   )
 }
-
 export default DocsHeader
