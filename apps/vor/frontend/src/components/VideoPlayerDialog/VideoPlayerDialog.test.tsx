3c89078232fa3667dd82613c3f8a7dc2cb84511c
Chrisando Pramudhita <chrsep@protonmail.com>
2021-01-17 02:43:36 +0700

    fix typecheck.

--- apps/vor/frontend/src/components/VideoPlayerDialog/VideoPlayerDialog.test.tsx
+++ apps/vor/frontend/src/components/VideoPlayerDialog/VideoPlayerDialog.test.tsx
@@ -1,15 +1,20 @@
+import dayjs from "dayjs"
+import { advanceTo } from "jest-date-mock"
 import React from "react"
 import { render } from "../../test-utils"
 import VideoPlayerDialog from "./VideoPlayerDialog"
 
 describe("VideoPlayerDialog", () => {
   it("should render correctly", () => {
+    advanceTo(new Date(2018, 5, 27, 0, 0, 0)) // r
+
     const { container } = render(
       <VideoPlayerDialog
         onClose={() => {}}
         src=""
         studentId=""
         thumbnailUrl=""
+        createdAt={dayjs().toISOString()}
       />
     )
     expect(container).toMatchSnapshot()
