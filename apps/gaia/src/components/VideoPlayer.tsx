b88b3ea0d214ae9e3d9c241541417ea28ec8dc0a
Chrisando Pramudhita <chrsep@protonmail.com>
2021-01-21 20:07:46 +0700

    add video player dialog.

---
+++ apps/gaia/src/components/VideoPlayer.tsx
@@ -0,0 +1,59 @@
+/* eslint-disable jsx-a11y/media-has-caption */
+import Hls from "hls.js/dist/hls.light"
+import React, { FC, useEffect, useRef } from "react"
+// @ts-ignore
+import mux from "mux-embed"
+
+export interface VideoPlayerProps {
+  src: string
+  poster?: string
+}
+const VideoPlayer: FC<VideoPlayerProps> = ({ src, poster }) => {
+  const video = useRef<HTMLVideoElement>(null)
+
+  useEffect(() => {
+    mux.monitor("#VideoPlayer", {
+      debug: false,
+      data: {
+        env_key:
+          process.env.NODE_ENV === "development"
+            ? "0pt3cncsn0aocpprvn2bntisq"
+            : "i2d2c3g7o9tt3tas4svflc1ce",
+        player_name: "Vor VideoPlayer",
+      },
+    })
+
+    let hls: Hls
+    if (video.current?.canPlayType("application/vnd.apple.mpegurl")) {
+      video.current.src = src
+    } else if (Hls.isSupported() && video.current) {
+      hls = new Hls()
+      hls.loadSource(src)
+      hls.attachMedia(video.current)
+
+      mux.addHLSJS("#VideoPlayer", { hlsjs: hls, Hls })
+    }
+
+    if (video.current && video.current.paused) {
+      video.current.play()
+    }
+
+    return () => {
+      if (hls !== undefined) {
+        hls.destroy()
+      }
+    }
+  }, [src, video.current])
+
+  return (
+    <video
+      id="VideoPlayer"
+      ref={video}
+      controls
+      className="w-full h-full"
+      poster={poster}
+    />
+  )
+}
+
+export default VideoPlayer
--- apps/gaia/src/pages/videos.tsx
+++ apps/gaia/src/pages/videos.tsx
@@ -1,7 +1,12 @@
-import React from "react"
+import React, { FC, Suspense, useState } from "react"
 import Image from "next/image"
+import Icon from "../components/Icon/Icon"
+import LoadingPlaceholder from "../components/LoadingPlaceholder/LoadingPlaceholder"
+import VideoPlayer from "../components/VideoPlayer"
 import useGetChildVideos from "../hooks/api/useGetChildVideos"
 import { useQueryString } from "../hooks/useQueryString"
+import Button from "../components/Button/Button"
+import { LazyVideoPlayer } from "../components/LazyVideoPlayer"
 
 const VideosPage = () => {
   const childId = useQueryString("childId")
@@ -9,18 +14,57 @@ const VideosPage = () => {
 
   return (
     <div className="max-w-3xl mx-auto">
-      {videos.data?.map((v) => {
-        return (
-          <Image
-            key={v.id}
-            src={v.thumbnailUrl}
-            width={200}
-            height={200}
-            objectFit="cover"
-          />
-        )
-      })}
+      <div className="flex">
+        {videos.data?.map((v) => {
+          return (
+            <Video
+              key={v.id}
+              thumbnailSrc={v.thumbnailUrl}
+              playbackUrl={v.playbackUrl}
+            />
+          )
+        })}
+      </div>
     </div>
+  )
+}
+
+const Video: FC<{ thumbnailSrc: string; playbackUrl: string }> = ({
+  thumbnailSrc,
+  playbackUrl,
+}) => {
+  const [showPlayer, setShowPlayer] = useState(false)
+
+  return (
+    <>
+      <Image
+        src={thumbnailSrc}
+        width={200}
+        height={200}
+        objectFit="cover"
+        className="cursor-pointer"
+        onClick={() => setShowPlayer(true)}
+      />
+      {showPlayer && (
+        <div className="bg-overlay fixed top-0 left-0 right-0 bottom-0 z-50 md:flex items-center">
+          <div className="bg-white md:max-w-lg md:mx-auto flex flex-col md:flex-row-reverse md:items-start md:rounded-lg overflow-hidden">
+            <div className="bg-white md:w-60 p-3 flex items-center items-start">
+              <div className="mr-auto">Find</div>
+              <Button
+                className="px-2 bg-white"
+                onClick={() => setShowPlayer(false)}
+              >
+                <Icon src="/icons/close.svg" size={20} />
+              </Button>
+            </div>
+
+            <Suspense fallback={<LoadingPlaceholder className="h-40 w-80" />}>
+              <LazyVideoPlayer src={playbackUrl} />
+            </Suspense>
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 
