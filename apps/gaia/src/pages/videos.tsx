8e36f0a4deedfb92872796540eb320866b4c7fca
Chrisando Pramudhita <chrsep@protonmail.com>
2021-01-22 18:36:07 +0700

    fix video layout.

--- apps/gaia/src/pages/videos.tsx
+++ apps/gaia/src/pages/videos.tsx
@@ -13,10 +13,10 @@ const VideosPage = () => {
 
   return (
     <div className="max-w-3xl mx-auto">
-      <div className="flex">
+      <div className="flex flex-wrap items-center">
         {videos.data?.map((v) => (
           <Video
-            key={v.id}
+            key={`${v.id}1`}
             thumbnailSrc={v.thumbnailUrl}
             playbackUrl={v.playbackUrl}
           />
@@ -34,18 +34,21 @@ const Video: FC<{ thumbnailSrc: string; playbackUrl: string }> = ({
 
   return (
     <>
-      <Image
-        src={thumbnailSrc}
-        width={200}
-        height={200}
-        objectFit="cover"
-        className="cursor-pointer"
-        onClick={() => setShowPlayer(true)}
-      />
+      <div className="w-1/3 md:w-1/5 relative">
+        <div className="w-full" style={{ paddingTop: "100%" }}>
+          <Image
+            src={thumbnailSrc}
+            layout="fill"
+            objectFit="cover"
+            className="cursor-pointer p-1"
+            onClick={() => setShowPlayer(true)}
+          />
+        </div>
+      </div>
       {showPlayer && (
         <div className="bg-overlay fixed top-0 left-0 right-0 bottom-0 z-50 md:flex items-center">
           <div className="bg-white md:max-w-lg md:mx-auto flex flex-col md:flex-row-reverse md:items-start md:rounded-lg overflow-hidden">
-            <div className="bg-white md:w-60 p-3 flex items-center items-start">
+            <div className="bg-white md:w-80 p-3 flex items-center items-start">
               <div className="mr-auto">Find</div>
               <Button
                 className="px-2 bg-white"
