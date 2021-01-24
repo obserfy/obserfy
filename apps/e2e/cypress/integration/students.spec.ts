bd03671c3c16f169222321df480ea67fa90ef06e
Chrisando Pramudhita <chrsep@protonmail.com>
2021-01-24 23:43:01 +0700

    simplify test even more.

--- apps/e2e/cypress/integration/students.spec.ts
+++ apps/e2e/cypress/integration/students.spec.ts
@@ -75,5 +75,21 @@ describe("test student features", function () {
     cy.visitVor("/dashboard/students")
     cy.contains(student2.name).click()
     cy.contains("Profile").click()
+
+    const newName = "jane"
+    cy.get("[aria-label=edit-name]").click()
+    cy.contains("label", "Name").find("input").clear().type(newName)
+    cy.contains("Save").click()
+    cy.contains(newName).should("be.visible")
+
+    cy.get('[aria-label="edit-gender"]').click()
+    cy.contains("label", "Gender").find("select").select("1")
+    cy.contains("Save").click()
+    cy.contains("Male").should("be.visible")
+
+    cy.get('[aria-label="edit-student id"]').click()
+    cy.contains("label", "Student ID").find("input").clear().type("test")
+    cy.contains("Save").click()
+    cy.contains("test").should("be.visible")
   })
 })
