a306a3c1e607669262f2f65e1203eb217f265363
Chrisando Pramudhita <chrsep@protonmail.com>
2021-01-25 19:42:10 +0700

    add test upload image for gaia.

--- apps/e2e/cypress/integration/images.spec.ts
+++ apps/e2e/cypress/integration/images.spec.ts
@@ -24,4 +24,21 @@ describe("test image features", function () {
     cy.visitVor("/")
     cy.get("[data-cy=profile-pic-image]").should("have.lengthOf", 1)
   })
+
+  it("should be able to upload image on gaia", function () {
+    cy.exec(`yarn run db:reset ${Cypress.env("GAIA_USERNAME")}`)
+    cy.createGuardian(this.student.id)
+      .then(() => cy.gaiaLogin())
+      .then(() => {
+        cy.visitGaia("/")
+        cy.contains(this.student.name).should("be.visible")
+        cy.contains("Images").click()
+        cy.get("#upload-image-small").attachFile("icon.png")
+        cy.get("[data-cy=image]").should("have.lengthOf", 1)
+        cy.get("#upload-image").attachFile("icon.png")
+        cy.get("[data-cy=image]").should("have.lengthOf", 2)
+        cy.get("[data-cy=image]").first().click()
+        cy.get("[data-cy=close]").click()
+      })
+  })
 })
