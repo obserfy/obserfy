2212f4637b5222b2c683cd3cfd655d96faae5fdc
Chrisando Pramudhita <chrsep@protonmail.com>
2021-02-10 18:16:05 +0700

    tweak font style.

--- apps/vor/frontend/src/components/PageCurriculumMaterial/PageCurriculumMaterial.tsx
+++ apps/vor/frontend/src/components/PageCurriculumMaterial/PageCurriculumMaterial.tsx
@@ -1,10 +1,9 @@
 import React, { FC } from "react"
+import { Box, ThemeUIStyleObject } from "theme-ui"
 
 export interface PageCurriculumMaterialProps {
   sx?: ThemeUIStyleObject
 }
-const PageCurriculumMaterial: FC<PageCurriculumMaterialProps> = () => (
-  <div></div>
-)
+const PageCurriculumMaterial: FC<PageCurriculumMaterialProps> = () => <Box />
 
 export default PageCurriculumMaterial
