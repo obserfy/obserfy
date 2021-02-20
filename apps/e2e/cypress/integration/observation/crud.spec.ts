e941ab8e165836125e7d0865c74c4e89c46bafb6
Chrisando Pramudhita <chrsep@protonmail.com>
2021-02-20 15:06:05 +0700

    fix vor and gaia build.

--- apps/e2e/cypress/integration/observation/crud.spec.ts
+++ apps/e2e/cypress/integration/observation/crud.spec.ts
@@ -1,4 +1,4 @@
-describe("test observation features", function () {
+describe("observation crud", function () {
   beforeEach(function () {
     cy.registerVor()
     cy.createStudent()
@@ -60,46 +60,5 @@ describe("test observation features", function () {
     cy.get("[data-cy=delete-observation]").click()
     cy.contains("Yes").click()
     cy.contains("A new description great").should("not.exist")
-  })
-
-  it("should be able to show observation to parents", function () {
-    cy.exec(`yarn run db:reset ${Cypress.env("GAIA_USERNAME")}`)
-    cy.createObservation(this.student.id, false)
-      .then(() => cy.createGuardian(this.student.id))
-      .then(() => cy.gaiaLogin())
-      .then(() => {
-        cy.visitGaia("/")
-        cy.contains(this.observation.shortDesc).should("not.exist")
-      })
-      .then(() => cy.createObservation(this.student.id, true))
-      .then(() => {
-        cy.visitGaia("/")
-        cy.contains(this.observation.shortDesc).should("be.visible")
-      })
-  })
-
-  it("should be able to post and delete observation to lesson plan", function () {
-    cy.exec(`yarn run db:reset ${Cypress.env("GAIA_USERNAME")}`)
-    cy.createLessonPlan(this.student.id)
-      .then(() => cy.createGuardian(this.student.id))
-      .then(() => cy.gaiaLogin())
-      .then(() => {
-        cy.visitGaia("/lesson-plan")
-        cy.contains(this.lessonPlan.title).should("not.exist")
-        cy.contains("Add observation").click()
-        cy.contains("label", "Observation").type("test")
-        cy.contains("Post").click()
-        cy.contains("test").should("be.visible")
-
-        cy.contains("Edit").click()
-        cy.contains("Edit observation").type(" another test")
-        cy.contains("Save").click()
-        cy.contains("test another test").should("be.visible")
-
-        cy.contains("Edit").click()
-        cy.get("[data-cy=delete-observation]").click({ force: true })
-        cy.contains("Yes").click()
-        cy.contains("test another test").should("not.exist")
-      })
   })
 })
