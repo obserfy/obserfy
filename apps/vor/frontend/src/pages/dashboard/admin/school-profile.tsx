d8aef486e4c26dd5057ada848c0c345a0b2ee70b
Chrisando Pramudhita <chrsep@protonmail.com>
2021-03-23 20:58:01 +0700

    add school name data box.

--- apps/vor/frontend/src/pages/dashboard/admin/school-profile.tsx
+++ apps/vor/frontend/src/pages/dashboard/admin/school-profile.tsx
@@ -1,7 +1,12 @@
 import React from "react"
+import PageSchoolProfile from "../../../components/PageSchoolProfile/PageSchoolProfile"
+import SEO from "../../../components/seo"
 
-const SchoolProfile = () => {
-  return <div>test</div>
-}
+const SchoolProfile = () => (
+  <>
+    <SEO title="School Profile" />
+    <PageSchoolProfile />
+  </>
+)
 
 export default SchoolProfile
