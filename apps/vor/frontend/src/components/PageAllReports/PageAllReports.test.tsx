5099468e935b8c7921009f3acb35dccab4a94cfc
Chrisando Pramudhita <chrsep@protonmail.com>
2021-03-29 20:06:12 +0700

    add empty illustration for reports.

---
+++ apps/vor/frontend/src/components/PageAllReports/PageAllReports.test.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { render } from "../../test-utils"
+import PageAllReports from "./PageAllReports"
+
+describe("PageAllReports", () => {
+  it("should render correctly", () => {
+    const { container } = render(<PageAllReports />)
+    expect(container).toMatchSnapshot()
+  })
+})
