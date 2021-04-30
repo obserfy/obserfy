00df298cf4e0d55aad736bf288423eacc95c3da5
Chrisando Pramudhita <chrsep@protonmail.com>
2021-04-30 17:12:19 +0700

    remove e2e unused deps.

--- apps/e2e/cypress/plugins/index.js
+++ apps/e2e/cypress/plugins/index.js
@@ -1,39 +1,9 @@
-/* eslint-disable @typescript-eslint/no-var-requires,global-require,import/no-extraneous-dependencies */
-const webpackPreprocessor = require("@cypress/webpack-preprocessor")
-const PnpWebpackPlugin = require("pnp-webpack-plugin")
-
-module.exports = (on) => {
-  // on("task", require("@cypress/code-coverage/task")(on, config))
-  const options = {
-    webpackOptions: {
-      module: {
-        rules: [
-          {
-            test: /\.ts$/,
-            use: [
-              {
-                loader: "swc-loader",
-                options: {
-                  jsc: {
-                    parser: {
-                      syntax: "typescript",
-                    },
-                  },
-                },
-              },
-            ],
-            exclude: /node_modules/,
-          },
-        ],
-      },
-      resolve: {
-        extensions: [".tsx", ".ts", ".js"],
-        plugins: [PnpWebpackPlugin],
-      },
-      resolveLoader: {
-        plugins: [PnpWebpackPlugin.moduleLoader(module)],
-      },
-    },
-  }
-  on("file:preprocessor", webpackPreprocessor(options))
+module.exports = (on, config) => {
+  on("before:browser:launch", (browser = {}, launchOptions) => {
+    if (browser.family === "firefox") {
+      // auto open devtools
+      launchOptions.args.push("-devtools")
+      return launchOptions
+    }
+  })
 }
