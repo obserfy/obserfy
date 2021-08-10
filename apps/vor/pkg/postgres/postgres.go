358d96904308f77811a5b5991add91458f909fb1
Chrisando Pramudhita <chrsep@protonmail.com>
2021-08-10 16:11:07 +0700

    add full report table.

--- apps/vor/pkg/postgres/postgres.go
+++ apps/vor/pkg/postgres/postgres.go
@@ -139,10 +139,12 @@ type Material struct {
 type StudentMaterialProgress struct {
 	MaterialId string   `pg:",pk,type:uuid,on_delete:CASCADE"`
 	Material   Material `pg:"rel:has-one"`
-	StudentId  string   `pg:",pk,type:uuid,on_delete:CASCADE"`
-	Student    Student  `pg:"rel:has-one"`
-	Stage      int      `pg:",notnull,use_zero"` // TODO: should be renamed to assessment
-	UpdatedAt  time.Time
+
+	StudentId string  `pg:",pk,type:uuid,on_delete:CASCADE"`
+	Student   Student `pg:"rel:has-one"`
+
+	Stage     int `pg:",notnull,use_zero"` // TODO: should be renamed to assessment
+	UpdatedAt time.Time
 }
 
 type Gender int
@@ -430,6 +432,70 @@ type (
 		Title       string
 		PeriodStart time.Time
 		PeriodEnd   time.Time
+		Published   bool
+	}
+
+	StudentReports struct {
+		Id              uuid.UUID `pg:"type:uuid"`
+		StudentId       uuid.UUID `pg:"type:uuid"`
+		GeneralComments string
+
+		AreaComments []StudentReportsAreaComments `pg:"rel:has-many"`
+		Published    bool
+	}
+
+	StudentReportsAreaComments struct {
+		Id uuid.UUID `pg:"type:uuid"`
+
+		StudentReportsId uuid.UUID
+		StudentReport    StudentReports `pg:"rel:has-one"`
+
+		AreaId uuid.UUID `pg:"type:uuid"`
+		Area   Area
+
+		Comments string
+		Ready    bool
+	}
+
+	StudentReportAssessments struct {
+		Id               uuid.UUID `pg:"type:uuid"`
+		StudentReportsId uuid.UUID `pg:"type:uuid"`
+		StudentReport    StudentReports
+
+		MaterialId string   `pg:"type:uuid,on_delete:CASCADE"`
+		Material   Material `pg:"rel:has-one"`
+
+		Assessments int `pg:",notnull,use_zero"`
+		UpdatedAt   time.Time
+	}
+
+	StudentReportObservations struct {
+		Id               uuid.UUID `pg:"type:uuid"`
+		StudentReportsId uuid.UUID `pg:"type:uuid"`
+		StudentReport    StudentReports
+
+		ShortDesc string `json:"shortDesc"`
+		LongDesc  string `json:"longDesc"`
+
+		StudentId   string    `pg:",type:uuid,on_delete:CASCADE"`
+		Student     *Student  `pg:"rel:has-one"`
+		CreatedDate time.Time `json:"createdDate"`
+		EventTime   time.Time
+
+		CreatorId string `pg:",type:uuid,on_delete:SET NULL"`
+		Creator   *User  `pg:"rel:has-one"`
+
+		LessonPlan   LessonPlan `pg:"rel:has-one"`
+		LessonPlanId string     `pg:"type:uuid,on_delete:SET NULL"`
+
+		Guardian   Guardian `pg:"rel:has-one"`
+		GuardianId string   `pg:"type:uuid,on_delete:SET NULL"`
+
+		Area   Area      `pg:"rel:has-one"`
+		AreaId uuid.UUID `pg:"type:uuid,on_delete:SET NULL"`
+
+		Images             []Image `pg:"many2many:observation_to_images,join_fk:image_id"`
+		VisibleToGuardians bool    `pg:",notnull,default:false"`
 	}
 )
 
