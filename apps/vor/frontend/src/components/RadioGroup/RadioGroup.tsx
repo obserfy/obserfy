8f2624a2a816dbf17e4682569ceca668bed85bb2
Chrisando Pramudhita <chrsep@protonmail.com>
2021-08-23 12:56:15 +0700

    suppress eslint for now.

--- apps/vor/frontend/src/components/RadioGroup/RadioGroup.tsx
+++ apps/vor/frontend/src/components/RadioGroup/RadioGroup.tsx
@@ -38,6 +38,7 @@ const RadioGroup: FC<RadioGroupsProps> = ({
       }}
     >
       {options.map(({ label, description }, idx) => (
+        // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-noninteractive-element-interactions
         <label
           key={label}
           htmlFor={label}
