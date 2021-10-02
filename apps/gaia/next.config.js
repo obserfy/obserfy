ea4e48419d075f3d5d0316d223598fcba4a512d8
Chrisando Pramudhita <chrsep@protonmail.com>
2021-10-02 16:56:26 +0700

    disable sentry and pwa plugin on dev.

--- apps/gaia/next.config.js
+++ apps/gaia/next.config.js
@@ -7,7 +7,11 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 })
 
-const plugins = [withSentryConfig, withPWA, withPreact, withBundleAnalyzer]
+const plugins = [withPreact, withBundleAnalyzer]
+if (process.env.NODE_ENV === "production") {
+  plugins.push(withPWA)
+  plugins.push(withSentryConfig)
+}
 
 const config = {
   env: {
