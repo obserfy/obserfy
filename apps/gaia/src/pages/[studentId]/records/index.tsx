99ff607076a8987a6e6f0e9fb25c1b4f7a90313c
Chrisando Pramudhita <chrsep@protonmail.com>
2021-10-07 13:53:51 +0700

    add placeholder for records page.

---
+++ apps/gaia/src/pages/[studentId]/records/index.tsx
@@ -0,0 +1,7 @@
+import BaseLayout from "$layouts/BaseLayout"
+
+const RecordsPage = () => {
+  return <BaseLayout title="Records | Observations">records</BaseLayout>
+}
+
+export default RecordsPage
