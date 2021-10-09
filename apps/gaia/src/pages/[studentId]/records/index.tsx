db4a0ef963c6d7970568b2a9bae237e0654b5650
Chrisando Pramudhita <chrsep@protonmail.com>
2021-10-09 17:29:49 +0700

    add controlled state to filters.

--- apps/gaia/src/pages/[studentId]/records/index.tsx
+++ apps/gaia/src/pages/[studentId]/records/index.tsx
@@ -1,3 +1,6 @@
+import { useRouter } from "next/router"
+import { ChangeEventHandler, useState } from "react"
+import { useQueryString } from "$hooks/useQueryString"
 import RecordsLayout from "$layouts/RecordsLayout"
 import { withAuthorization } from "$lib/auth"
 import {
@@ -7,109 +10,166 @@ import {
 import { getStudentId, SSR } from "$lib/next"
 import dayjs from "$lib/dayjs"
 
+const today = dayjs()
+
 const RecordsPage: SSR<typeof getServerSideProps> = ({
   observations,
   areas,
-}) => (
-  <RecordsLayout title="Observations">
-    <div className="sm:flex items-start m-4">
-      <div className="hidden lg:block sticky top-20 flex-shrink-0 mr-4 w-1/3 rounded-xl">
-        <div>
-          <label
-            htmlFor="location"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Area
-          </label>
-          <select
-            id="location"
-            name="location"
-            className="block py-2 pr-10 pl-3 mt-1 w-full text-base sm:text-sm rounded-md border-gray-200 focus:border-primary-500 focus:ring-primary-500 shadow-sm focus:outline-none"
-            defaultValue="Canada"
-          >
-            {areas.map(({ id, name }) => (
-              <option key={id}>{name}</option>
-            ))}
-          </select>
-        </div>
+  oldestObservationDate,
+}) => {
+  const router = useRouter()
+  const areaQuery = useQueryString("area")
+  const fromQuery = useQueryString("from")
+  const toQuery = useQueryString("to")
 
-        <div className="isolate mt-4 -space-y-px bg-white rounded-md shadow-sm">
-          <div className="relative focus-within:z-10 py-2 px-3 rounded-md rounded-b-none border focus-within:border-primary-600 focus-within:ring-1 focus-within:ring-primary-600">
+  const [area, setArea] = useState(areaQuery ?? "all")
+  const [from, setFrom] = useState(
+    fromQuery ? dayjs(fromQuery) : dayjs(oldestObservationDate)
+  )
+  const [to, setTo] = useState(toQuery ? dayjs(toQuery) : today)
+
+  const updateQuery = async (query: any) => {
+    await router.push({
+      pathname: router.pathname,
+      query: { ...router.query, ...query },
+    })
+  }
+
+  const handleAreaChange: ChangeEventHandler<HTMLSelectElement> = async (e) => {
+    await updateQuery({ area: e.target.value })
+    setArea(e.target.value)
+  }
+
+  const handleFromChange: ChangeEventHandler<HTMLInputElement> = async (e) => {
+    const value = dayjs(e.target.value)
+    await updateQuery({ from: value.format("YYYY-MM-DD") })
+    setFrom(value)
+  }
+
+  const handleToChange: ChangeEventHandler<HTMLInputElement> = async (e) => {
+    const value = dayjs(e.target.value)
+    await updateQuery({ to: value.format("YYYY-MM-DD") })
+    setTo(value)
+  }
+
+  return (
+    <RecordsLayout title="Observations">
+      <div className="sm:flex items-start m-4">
+        <div className="hidden lg:block sticky top-20 flex-shrink-0 mr-4 w-1/3 rounded-xl">
+          <div>
             <label
-              htmlFor="name"
+              htmlFor="areas"
               className="block text-sm font-medium text-gray-700"
             >
-              From
+              Area
             </label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              className="block p-0 placeholder-gray-500 text-gray-900 border-0 focus:ring-0"
-              placeholder="26 Jan 2016"
-            />
-          </div>
-          <div className="relative focus-within:z-10 py-2 px-3 rounded-md rounded-t-none border focus-within:border-primary-600 focus-within:ring-1 focus-within:ring-primary-600">
-            <label
-              htmlFor="job-title"
-              className="block w-full text-sm font-medium text-gray-700"
+            <select
+              id="areas"
+              name="areas"
+              className="block py-2 pr-10 pl-3 mt-1 w-full text-base sm:text-sm rounded-md border-gray-200 focus:border-primary-500 focus:ring-primary-500 shadow-sm focus:outline-none"
+              defaultValue={area}
+              value={area}
+              onChange={handleAreaChange}
             >
-              To
-            </label>
-            <input
-              type="text"
-              name="job-title"
-              id="job-title"
-              className="block p-0 w-full placeholder-gray-500 text-gray-900 border-0 focus:ring-0"
-              placeholder="21 Sep 2021"
-            />
+              <option value="all">All</option>
+              {areas.map(({ id, name }) => (
+                <option key={id} value={id}>
+                  {name}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div className="isolate mt-4 -space-y-px bg-white rounded-md shadow-sm">
+            <div className="relative focus-within:z-10 py-2 px-3 rounded-md rounded-b-none border focus-within:border-primary-600 focus-within:ring-1 focus-within:ring-primary-600">
+              <label
+                htmlFor="date-from"
+                className="block text-sm font-medium text-gray-700"
+              >
+                From
+              </label>
+              <input
+                type="date"
+                name="date-from"
+                id="date-from"
+                className="block p-0 w-full placeholder-gray-500 text-gray-900 border-0 focus:ring-0"
+                value={from.format("YYYY-MM-DD")}
+                min={dayjs(oldestObservationDate).format("YYYY-MM-DD")}
+                max={to.format("YYYY-MM-DD")}
+                onChange={handleFromChange}
+              />
+            </div>
+            <div className="relative focus-within:z-10 py-2 px-3 rounded-md rounded-t-none border focus-within:border-primary-600 focus-within:ring-1 focus-within:ring-primary-600">
+              <label
+                htmlFor="date-from"
+                className="block w-full text-sm font-medium text-gray-700"
+              >
+                To
+              </label>
+              <input
+                type="date"
+                name="date-to"
+                id="date-to"
+                className="p-0 w-full placeholder-gray-500 text-gray-900 border-0 focus:ring-0"
+                value={to.format("YYYY-MM-DD")}
+                min={from.format("YYYY-MM-DD")}
+                max={today.format("YYYY-MM-DD")}
+                onChange={handleToChange}
+              />
+            </div>
           </div>
         </div>
+
+        <ul className="overflow-hidden rounded-xl border divide-y divide-gray-200 shadow-sm">
+          {observations.map(
+            ({ id, short_desc, long_desc, event_time, areas: a }) => (
+              <li
+                key={id}
+                className="relative py-5 px-4 bg-white hover:bg-primary-50 focus-within:ring-2 focus-within:ring-inset focus-within:ring-primary-600"
+              >
+                <div className="flex justify-between space-x-3">
+                  <a href="#" className="block truncate focus:outline-none">
+                    <span className="absolute inset-0" aria-hidden="true" />
+                    <p className="font-semibold text-gray-900 truncate">
+                      {short_desc}
+                    </p>
+                    <p className="font-semibold text-primary-600 truncate">
+                      {a?.name ?? "Others"}
+                    </p>
+                  </a>
+
+                  <time
+                    dateTime={event_time}
+                    className="flex flex-shrink-0 text-gray-500 whitespace-nowrap"
+                  >
+                    {dayjs(event_time).format("DD MMM YYYY")}
+                  </time>
+                </div>
+                <div className="mt-1">
+                  <p className="text-gray-600 line-clamp-2">{long_desc}</p>
+                </div>
+              </li>
+            )
+          )}
+        </ul>
       </div>
-
-      <ul className="overflow-hidden rounded-xl border divide-y divide-gray-200 shadow-sm">
-        {observations.map(
-          ({ id, short_desc, long_desc, event_time, areas: a }) => (
-            <li
-              key={id}
-              className="relative py-5 px-4 bg-white hover:bg-primary-50 focus-within:ring-2 focus-within:ring-inset focus-within:ring-primary-600"
-            >
-              <div className="flex justify-between space-x-3">
-                <a href="#" className="block truncate focus:outline-none">
-                  <span className="absolute inset-0" aria-hidden="true" />
-                  <p className="font-semibold text-gray-900 truncate">
-                    {short_desc}
-                  </p>
-                  <p className="font-semibold text-primary-600 truncate">
-                    {a?.name ?? "Others"}
-                  </p>
-                </a>
-
-                <time
-                  dateTime={event_time}
-                  className="flex flex-shrink-0 text-gray-500 whitespace-nowrap"
-                >
-                  {dayjs(event_time).format("DD MMM YYYY")}
-                </time>
-              </div>
-              <div className="mt-1">
-                <p className="text-gray-600 line-clamp-2">{long_desc}</p>
-              </div>
-            </li>
-          )
-        )}
-      </ul>
-    </div>
-  </RecordsLayout>
-)
+    </RecordsLayout>
+  )
+}
 
 export const getServerSideProps = withAuthorization(async (ctx) => {
   const studentId = getStudentId(ctx)
   const observations = await findStudentObservations(studentId)
   const areas = await findCurriculumAreasByStudentId(studentId)
 
+  const firstDate =
+    observations.length > 0
+      ? observations[observations.length - 1].event_time?.toISOString()
+      : undefined
+
   return {
     props: {
+      oldestObservationDate: firstDate,
       areas: areas ?? [],
       observations: observations.map((o) => ({
         ...o,
