97991c8ba48b1f4e0e89dc903563a8af368692bd
Chrisando E. Pramudhita <chrsep@protonmail.com>
2022-07-27 13:12:01 +0700

    speed up slide over transition.

--- apps/gaia/src/components/SlideOver.tsx
+++ apps/gaia/src/components/SlideOver.tsx
@@ -32,10 +32,10 @@ const SlideOver: FC<{
         <div className="fixed inset-y-0 right-0 flex max-w-full pl-10">
           <Transition.Child
             as={Fragment}
-            enter="transform transition ease-in-out duration-500 sm:duration-700"
+            enter="transform transition ease-in-out duration-500 sm:duration-200"
             enterFrom="translate-x-full"
             enterTo="translate-x-0"
-            leave="transform transition ease-in-out duration-500 sm:duration-700"
+            leave="transform transition ease-in-out duration-500 sm:duration-100"
             leaveFrom="translate-x-0"
             leaveTo="translate-x-full"
           >
