e5e0422d0d7ca15759eb02483ab9da20e4dacb4b
Chrisando E. Pramudhita <chrsep@protonmail.com>
2022-07-30 15:12:44 +0700

    allow editing lesson plan observation.

--- apps/gaia/src/components/LessonPlanDetailSlideOver/observation.tsx
+++ apps/gaia/src/components/LessonPlanDetailSlideOver/observation.tsx
@@ -1,17 +1,18 @@
+import planId from "$api/plans/[planId]"
+import Button from "$components/Button/Button"
+import Icon from "$components/Icon/Icon"
+import usePatchPlanObservation from "$hooks/api/usePatchPlanObservation"
+import usePostPlanObservation from "$hooks/api/usePostPlanObservation"
+import useToggle from "$hooks/useToggle"
+import dayjs, { Dayjs } from "$lib/dayjs"
 import { Menu, Transition } from "@headlessui/react"
-import clsx from "clsx"
-import { FC, Fragment, useState } from "react"
 import {
   guardians as Guardians,
   observations as Observations,
   users as Users,
 } from "@prisma/client"
-import dayjs, { Dayjs } from "$lib/dayjs"
-import AlertDialog from "$components/AlertDialog"
-import Button from "$components/Button/Button"
-import Icon from "$components/Icon/Icon"
-import usePostPlanObservation from "$hooks/api/usePostPlanObservation"
-import useToggle from "$hooks/useToggle"
+import clsx from "clsx"
+import { FC, Fragment, useState } from "react"
 
 export const ObservationsList: FC<{
   lessonPlanId: string
@@ -28,10 +29,12 @@ export const ObservationsList: FC<{
         {observations.map((o) => (
           <Observation
             key={o.id}
+            observationId={o.id}
             long_desc={o.long_desc}
             event_time={dayjs(o.event_time)}
             userName={o.users?.name}
             guardianName={o.guardians?.name}
+            lessonPlanId={lessonPlanId}
           />
         ))}
       </ul>
@@ -42,13 +45,33 @@ export const ObservationsList: FC<{
 )
 
 const Observation: FC<{
+  observationId: string
   event_time: Dayjs
   long_desc: string | null
   userName?: string | null
   guardianName?: string | null
-}> = ({ event_time, long_desc, guardianName, userName }) => {
+  lessonPlanId: string
+}> = ({
+  lessonPlanId,
+  event_time,
+  long_desc,
+  guardianName,
+  userName,
+  observationId,
+}) => {
   const deleteDialog = useToggle()
   const editForm = useToggle()
+
+  if (editForm.isOn) {
+    return (
+      <EditObservation
+        lessonPlanId={lessonPlanId}
+        observationId={observationId}
+        onDismiss={editForm.toggle}
+        original={long_desc ?? ""}
+      />
+    )
+  }
 
   return (
     <li className="mb-2 rounded-xl border border-gray-200 bg-white p-4">
@@ -61,7 +84,10 @@ const Observation: FC<{
           />
         )}
 
-        <ObservationDropdown onDeleteClick={() => {}} onEditClick={() => {}} />
+        <ObservationDropdown
+          onDeleteClick={deleteDialog.toggle}
+          onEditClick={editForm.toggle}
+        />
       </div>
 
       <div className="flex">
@@ -77,38 +103,73 @@ const Observation: FC<{
   )
 }
 
-const DeleteObservationDialog: FC<{
-  isOpen: boolean
-  close: () => void
-}> = ({ isOpen, close }) => (
-  <AlertDialog
-    isOpen={isOpen}
-    close={close}
-    title="Delete Observation"
-    icon={
-      <a>
-        <Button className="w-full !bg-red-600 text-base hover:!bg-red-700 focus:!ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">
-          Sign Out
-        </Button>
-      </a>
-    }
-    actionButton={
-      <div className="mx-auto flex h-12 w-12 shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-        <Icon
-          src="/icons/log-out.svg"
-          className="h-6 w-6"
-          color="bg-red-600"
-          aria-hidden="true"
+export const EditObservation: FC<{
+  lessonPlanId: string
+  observationId: string
+  onDismiss: () => void
+  original: string
+}> = ({ lessonPlanId, observationId, original, onDismiss }) => {
+  const [observation, setObservation] = useState(original)
+
+  const { mutateAsync, isLoading } = usePatchPlanObservation(
+    lessonPlanId,
+    observationId
+  )
+
+  return (
+    <div className="flex h-60 flex-col overflow-hidden rounded-xl border border-gray-200 bg-white ring-primary-500 focus-within:ring-2">
+      <label
+        htmlFor="new-observation"
+        className="h-full w-full outline-none focus:outline-none"
+      >
+        <span className="sr-only">Observation</span>
+        <textarea
+          id="new-observation"
+          placeholder="Post an observation..."
+          className="h-full w-full resize-none border-none p-4 placeholder:text-gray-400 focus:!outline-none"
+          value={observation}
+          onChange={(e) => {
+            setObservation(e.target.value)
+          }}
         />
+      </label>
+
+      <div className={"m-2 flex"}>
+        <Button
+          variant={"outline"}
+          className="ml-auto text-red-900"
+          onClick={onDismiss}
+        >
+          Cancel
+        </Button>
+        <Button
+          className="ml-2"
+          disabled={observation === "" || isLoading}
+          onClick={async () => {
+            if (observation) {
+              await mutateAsync({ observation })
+              onDismiss()
+            }
+          }}
+        >
+          {isLoading && (
+            <Icon
+              src="/icons/spinner.svg"
+              color="bg-white"
+              className="!h-4 !w-4 animate-spin"
+            />
+          )}
+          Submit
+        </Button>
       </div>
-    }
-  />
-)
+    </div>
+  )
+}
 
 const ObservationDropdown: FC<{
   onEditClick: () => void
   onDeleteClick: () => void
-}> = () => (
+}> = ({ onDeleteClick, onEditClick }) => (
   <Menu as="div" className="relative ml-auto inline-block text-left">
     <div>
       <Menu.Button className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white p-1 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2 focus:ring-offset-gray-100">
@@ -129,12 +190,12 @@ const ObservationDropdown: FC<{
         <div className="py-1">
           <Menu.Item>
             {({ active }) => (
-              <a
-                href="#"
+              <button
                 className={clsx(
                   active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                  "flex items-center py-2 px-4 text-sm"
+                  "flex items-center py-2 px-4 text-sm w-full"
                 )}
+                onClick={onEditClick}
               >
                 <Icon
                   src="/icons/edit.svg"
@@ -142,17 +203,17 @@ const ObservationDropdown: FC<{
                   color="bg-gray-500"
                 />
                 Edit
-              </a>
+              </button>
             )}
           </Menu.Item>
           <Menu.Item>
             {({ active }) => (
-              <a
-                href="#"
+              <button
                 className={clsx(
                   active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                  "flex items-center py-2 px-4 text-sm"
+                  "flex items-center py-2 px-4 text-sm w-full"
                 )}
+                onClick={onDeleteClick}
               >
                 <Icon
                   src="/icons/trash.svg"
@@ -160,7 +221,7 @@ const ObservationDropdown: FC<{
                   color="bg-gray-500"
                 />
                 Delete
-              </a>
+              </button>
             )}
           </Menu.Item>
         </div>
---
+++ apps/gaia/src/hooks/api/usePatchPlanObservation.ts
@@ -0,0 +1,19 @@
+import { PatchObservationRequestBody } from "$api/observations/[observationId]"
+import { track } from "$lib/mixpanel"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { patchApi } from "./apiHelpers"
+
+const usePostPlanObservation = (planId: string, observationId: string) => {
+  const queryClient = useQueryClient()
+  const postPlanObservation = patchApi<PatchObservationRequestBody>(
+    `/observations/${observationId}`
+  )
+  return useMutation(postPlanObservation, {
+    onSuccess: async () => {
+      track("Observation Created")
+      await queryClient.invalidateQueries(["lesson-plan", planId])
+    },
+  })
+}
+
+export default usePostPlanObservation
